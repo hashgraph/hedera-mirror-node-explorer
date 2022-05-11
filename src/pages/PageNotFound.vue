23dc191a00773b8d5282fd5e906687058dce492b
Eric Le Ponner <eric.leponner@icloud.com>
2022-05-11 20:04:35 +0200

    Fix for issue #6 (proper 404 page).
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

---
+++ src/pages/PageNotFound.vue
@@ -0,0 +1,69 @@
+<!--
+  -
+  - Hedera Mirror Node Explorer
+  -
+  - Copyright (C) 2021 - 2022 Hedera Hashgraph, LLC
+  -
+  - Licensed under the Apache License, Version 2.0 (the "License");
+  - you may not use this file except in compliance with the License.
+  - You may obtain a copy of the License at
+  -
+  -      http://www.apache.org/licenses/LICENSE-2.0
+  -
+  - Unless required by applicable law or agreed to in writing, software
+  - distributed under the License is distributed on an "AS IS" BASIS,
+  - WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  - See the License for the specific language governing permissions and
+  - limitations under the License.
+  -
+  -->
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                     TEMPLATE                                                    -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<template>
+
+  <hr class="h-top-banner" style="margin: 0; height: 4px"/>
+
+  <section class="section has-text-centered" style="min-height: 450px">
+
+    <p class="h-is-secondary-text" style="font-weight: 300">Page not found</p>
+    <br/>
+
+    <div class="block">
+      <p class="h-is-tertiary-text" style="font-weight: 300">
+        No page matches the specified URL.
+        <br>
+        Check URL value or use tabs in navigation bar.
+      </p>
+    </div>
+
+  </section>
+
+  <Footer :keep-background="true"/>
+
+</template>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                      SCRIPT                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<script lang="ts">
+
+import {defineComponent} from 'vue';
+import Footer from "@/components/Footer.vue";
+
+export default defineComponent({
+  name: 'PageNotFound',
+  components: {Footer}
+})
+
+</script>
+
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                       STYLE                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<style scoped/>
---
+++ tests/e2e/specs/PageNotFound.spec.ts
@@ -0,0 +1,105 @@
+/*-
+ *
+ * Hedera Mirror Node Explorer
+ *
+ * Copyright (C) 2021 - 2022 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// https://docs.cypress.io/api/introduction/api.html
+
+describe('Hedera Explorer page not found', () => {
+
+  let target = "No page matches the specified URL"
+
+  beforeEach( () => {
+    localStorage.removeItem("network")
+    cy.visit('/')
+    cy.url().should('include', '/testnet/dashboard')
+  })
+
+  //
+  // last used network is undefined
+  //
+
+  it('/#/page-not-found (last used network is undefined)', () => {
+    cy.visit('/#/page-not-found')
+    cy.url().should('include', '/#/testnet/page-not-found')
+    cy.contains(target)
+  })
+
+  it('/#/testnet/page-not-found (last used network is undefined)', () => {
+    cy.visit('/#/testnet/page-not-found')
+    cy.url().should('include', '/#/testnet/page-not-found')
+    cy.contains(target)
+  })
+
+  it('/#/mainnet/page-not-found (last used network is undefined)', () => {
+    cy.visit('/#/mainnet/page-not-found')
+    cy.url().should('include', '/#/mainnet/page-not-found')
+    cy.contains(target)
+  })
+
+  //
+  // last used network is testnet
+  //
+
+  it('/#/page-not-found (last used network is tesnet)', () => {
+    localStorage.setItem("network", "tesnet")
+    cy.visit('/#/page-not-found')
+    cy.url().should('include', '/#/testnet/page-not-found')
+    cy.contains(target)
+  })
+
+  it('/#/testnet/page-not-found (last used network is tesnet)', () => {
+    localStorage.setItem("network", "tesnet")
+    cy.visit('/#/testnet/page-not-found')
+    cy.url().should('include', '/#/testnet/page-not-found')
+    cy.contains(target)
+  })
+
+  it('/#/mainnet/page-not-found (last used network is tesnet)', () => {
+    localStorage.setItem("network", "tesnet")
+    cy.visit('/#/mainnet/page-not-found')
+    cy.url().should('include', '/#/mainnet/page-not-found')
+    cy.contains(target)
+  })
+
+  //
+  // last used network is mainnet
+  //
+
+  it('/#/page-not-found (last used network is mainnet)', () => {
+    localStorage.setItem("network", "mainnet")
+    cy.visit('/#/page-not-found')
+    cy.url().should('include', '/#/testnet/page-not-found')
+    cy.contains(target)
+  })
+
+  it('/#/testnet/page-not-found (last used network is mainnet)', () => {
+    localStorage.setItem("network", "mainnet")
+    cy.visit('/#/testnet/page-not-found')
+    cy.url().should('include', '/#/testnet/page-not-found')
+    cy.contains(target)
+  })
+
+  it('/#/testnet/page-not-found (last used network is mainnet)', () => {
+    localStorage.setItem("network", "mainnet")
+    cy.visit('/#/mainnet/page-not-found')
+    cy.url().should('include', '/#/mainnet/page-not-found')
+    cy.contains(target)
+  })
+
+})
