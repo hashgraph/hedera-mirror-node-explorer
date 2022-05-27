4aa6cf325a9d8f8228c8ac01bc792be9d1f95900
Eric Le Ponner <eric.leponner@icloud.com>
2022-05-27 14:59:42 +0200

    Contract details view now uses ByteCodeValue component to display "Code" field.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

---
+++ src/components/values/ByteCodeValue.vue
@@ -0,0 +1,66 @@
+<!--
+  -
+  - Hedera Mirror Node Explorer
+  -
+  - Copyright (C) 2021 - 2022 Hedera Hashgraph, LLC
+  -
+  - Licensed under the Apache License, Version 2.0 (the "License");
+  - you may not use this file except in compliance with the License.
+  - You may obtain a copy of the License at
+  -
+  -      http://www.apache.org/licenses/LICENSE-2.0
+  -
+  - Unless required by applicable law or agreed to in writing, software
+  - distributed under the License is distributed on an "AS IS" BASIS,
+  - WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  - See the License for the specific language governing permissions and
+  - limitations under the License.
+  -
+  -->
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                     TEMPLATE                                                    -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<template>
+
+  <textarea v-if="byteCode"
+            v-model="byteCode"
+            readonly rows="4"
+            style="width:100%; font-family: novamonoregular,monospace"></textarea>
+
+  <span v-else-if="initialLoading"/>
+
+  <span v-else class="has-text-grey">None</span>
+
+</template>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                      SCRIPT                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<script lang="ts">
+
+import {defineComponent, inject, ref} from 'vue';
+import {initialLoadingKey} from "@/AppKeys";
+
+export default defineComponent({
+  name: 'ByteCodeValue',
+
+  props: {
+    byteCode: String,
+  },
+
+  setup() {
+    const initialLoading = inject(initialLoadingKey, ref(false))
+    return { initialLoading }
+  }
+});
+
+</script>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                       STYLE                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<style/>
