20c7d1c3eaa073beb776797fb0d9d1e1a74fc16a
Simon Viénot <simon.vienot@icloud.com>
2022-06-06 15:04:56 +0200

    Display node service endpoints.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

---
+++ src/components/values/Endpoint.vue
@@ -0,0 +1,65 @@
+<!--
+  -
+  - Hedera Mirror Node Explorer
+  -
+  - Copyright (C) 2021 - 2022 Hedera Hashgraph, LLC
+  -
+  - Licensed under the Apache License, Version 2.0 (the "License");
+  - you may not use this file except in compliance with the License.
+  - You may obtain a copy of the License at
+  -
+  -      http://www.apache.org/licenses/LICENSE-2.0
+  -
+  - Unless required by applicable law or agreed to in writing, software
+  - distributed under the License is distributed on an "AS IS" BASIS,
+  - WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  - See the License for the specific language governing permissions and
+  - limitations under the License.
+  -
+  -->
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                     TEMPLATE                                                    -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<template>
+
+  <span v-if="address">
+    <span>{{ address }}</span>
+    <span v-if="port" class="has-text-grey h-is-smaller">{{ ':' + port }}</span>
+  </span>
+
+  <span v-else/>
+
+</template>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                      SCRIPT                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<script lang="ts">
+
+import {defineComponent, inject, ref} from 'vue';
+import {initialLoadingKey} from "@/AppKeys";
+
+export default defineComponent({
+  name: 'Endpoint',
+
+  props: {
+    address: String,
+    port: Number,
+  },
+
+  setup() {
+    const initialLoading = inject(initialLoadingKey, ref(false))
+    return {initialLoading}
+  }
+});
+
+</script>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                       STYLE                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<style/>
