hashgraph/hedera-mirror-node-explorer

commit 5f8552cfaf1982c24ba5ef78c403537514bb00eb

000088 blank page when cookies blocked (#99)
* Explorer now works even if LocalStorage is disabled by navigator (#88).

Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

* Fixed network selection when Dashboard is the currently displayed page.

Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

* Updating network in navigator URL field is now correctly supported (it redirects to dashboard tab).

Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

* Replace 'npm ci' by 'npm install' in GH actions to work-around current build failures.

Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

Co-authored-by: Simon Viénot <simon.vienot@icloud.com>

src/router.ts
@@ -18,7 +18,7 @@
  *
  */
 
-import {createRouter, createWebHashHistory, RouteRecordRaw} from 'vue-router'
+import {createRouter, createWebHashHistory, RouteLocationNormalized, RouteRecordRaw} from 'vue-router'
 import MainDashboard from "@/pages/MainDashboard.vue";
 import Transactions from "@/pages/Transactions.vue";
 import TransactionDetails from "@/pages/TransactionDetails.vue";
@@ -37,16 +37,18 @@ import {AxiosMonitor} from "@/utils/AxiosMonitor";
 import TransactionsById from "@/pages/TransactionsById.vue";
 import MobileMenu from "@/pages/MobileMenu.vue";
 import MobileSearch from "@/pages/MobileSearch.vue";
-import {networkRegistry} from "@/schemas/NetworkRegistry";
+import {NetworkEntry, networkRegistry} from "@/schemas/NetworkRegistry";
+import {AppStorage} from "@/AppStorage";
+import axios from "axios";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/' + networkRegistry.getLastUsedNetwork() + '/dashboard'
+    redirect: '/' + AppStorage.getLastNetwork().name  + '/dashboard'
   },
   {
     path: '/page-not-found',
-    redirect: '/' + networkRegistry.getLastUsedNetwork() + '/page-not-found'
+    redirect: '/' + AppStorage.getLastNetwork().name + '/page-not-found'
   },
   {
     path: '/:network/page-not-found',
@@ -169,30 +171,30 @@ const router = createRouter({
 })
 
 router.beforeEach((to, from) => {
-  const fromNetwork = networkRegistry.getLastUsedNetwork()
-  const toNetwork = to.params.network as string
+  let result: boolean | string
 
-  let result
-
-  if(networkRegistry.lookup(toNetwork)) {
-
-    networkRegistry.useNetwork(toNetwork)
-
-    if (fromNetwork && (fromNetwork != toNetwork) && (from.name == to.name)) {
-      result = {
-        name: 'MainDashboard',
-        params: {
-          network: toNetwork
-        }
+  const toEntry = getNetworkEntryFromRoute(to)
+  const fromEntry = getNetworkEntryFromRoute(from) ?? networkRegistry.getDefaultEntry()
+  if (toEntry !== null) {
+    // Network is valid
+    if (fromEntry != toEntry) {
+      // Network is changing => updates AppStorage and axios
+      AppStorage.setLastNetwork(toEntry)
+      axios.defaults.baseURL = toEntry.url
+      if (to.name != "MainDashboard") {
+        // We re-route on MainDashboard
+        result = "/" + toEntry.name + "/dashboard"
       }
-    } else {
-      result = true
-    }
+      else {
+        result = true
+      }
+    } else (
+        result = true
+    )
   } else {
-    // console.log("Unknown network: <" + toNetwork + ">")
+    // Network is invalid => page not found
     result = '/page-not-found'
   }
-
   return result
 })
 
@@ -237,3 +239,20 @@ router.beforeEach(() => {
 })
 
 export default router
+
+export function getNetworkEntryFromRoute(r: RouteLocationNormalized): NetworkEntry | null {
+
+  let networkName: string|null
+  const networkParam = r.params.network
+  if (Array.isArray(networkParam)) {
+    networkName = networkParam.length >= 1 ? networkParam[0] : null
+  } else {
+    networkName = networkParam
+  }
+
+  return networkName !== null ? networkRegistry.lookup(networkName) : null
+}
+
+export function getNetworkEntryFromCurrentRoute(): NetworkEntry {
+  return getNetworkEntryFromRoute(router.currentRoute.value) ?? networkRegistry.getDefaultEntry()
+}

src/AppStorage.ts
@@ -1,0 +1,48 @@
+/*-
+ *
+ * Hedera Mirror Node Explorer
+ *
+ * Copyright (C) 2021 - 2022 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+import {NetworkEntry, networkRegistry} from "@/schemas/NetworkRegistry";
+
+export class AppStorage {
+
+    private static readonly LAST_USED_NETWORK_KEY = 'network'
+
+    public static getLastNetwork(): NetworkEntry {
+        let result: NetworkEntry|null
+        try {
+            const item = localStorage.getItem(AppStorage.LAST_USED_NETWORK_KEY)
+            result = item !== null ? networkRegistry.lookup(item) : null
+        } catch {
+            // Navigator is setup to block all cookies => we'll use an hardcoded fallback
+            result = null
+        }
+        return result ?? networkRegistry.getDefaultEntry()
+    }
+
+    public static setLastNetwork(newValue: string|NetworkEntry): void {
+        const newItem = typeof newValue == "string" ? newValue : newValue.name
+        try {
+            localStorage.setItem(AppStorage.LAST_USED_NETWORK_KEY, newItem);
+        } catch {
+            // Navigator is setup to block all cookies => we forget last used network
+        }
+    }
+
+}

src/components/TopNavBar.vue
@@ -160,7 +160,7 @@ export default defineComponent({
       selectedNetwork.value = newValue as string
       selectedNetworkWatchHandle = watch(selectedNetwork, (selection) => {
         router.push({
-          name: name.value as string,
+          name: "MainDashboard",
           params: { network: selection }
         })
       })

src/pages/MainDashboard.vue
@@ -140,7 +140,8 @@ export default defineComponent({
     const ContractCallTransactionCacheState = ref<PlayPauseState>(PlayPauseState.Play)
 
     watch(() => props.network, () => {
-      window.location.href = "/";
+      // We don't want to wait for table periodic refresh => we trigger a full reload
+      window.location.reload();
     })
 
     return {

src/schemas/NetworkRegistry.ts
@@ -18,8 +18,6 @@
  *
  */
 
-import axios from "axios";
-
 export class NetworkEntry {
 
     public readonly name: string
@@ -35,7 +33,6 @@ export class NetworkEntry {
 
 export class NetworkRegistry {
 
-    private static readonly LAST_USED_NETWORK_KEY = 'network'
     private static readonly DEFAULT_NETWORK = 'testnet'
     private readonly defaultEntry: NetworkEntry
 
@@ -73,18 +70,12 @@ export class NetworkRegistry {
         return this.entries
     }
 
+    public getDefaultEntry(): NetworkEntry {
+        return this.defaultEntry
+    }
+
     public lookup(name: string): NetworkEntry | null {
         return this.entries.find(element => element.name === name) ?? null
-    }
-
-    public getLastUsedNetwork(): string {
-        return localStorage.getItem(NetworkRegistry.LAST_USED_NETWORK_KEY) ?? this.defaultEntry.name
-    }
-
-    public useNetwork(network: string): void {
-        const newEntry = this.lookup(network) ?? this.defaultEntry
-        localStorage.setItem(NetworkRegistry.LAST_USED_NETWORK_KEY, newEntry.name);
-        axios.defaults.baseURL = newEntry.url
     }
 }
 

src/schemas/OperatorRegistry.ts
@@ -48,7 +48,7 @@
 
  */
 
-import {networkRegistry} from "@/schemas/NetworkRegistry";
+import {getNetworkEntryFromCurrentRoute} from "@/router";
 import {EntityID} from "@/utils/EntityID";
 
 export class OperatorEntry {
@@ -115,7 +115,7 @@ export class OperatorRegistry {
 
     public lookup(accountId: string): OperatorEntry|null {
         const result = this.entries.get(accountId)
-        const network = networkRegistry.getLastUsedNetwork()
+        const network = getNetworkEntryFromCurrentRoute().name
         return result && (result.network == network || result.network == null) ? result : null
     }
 

commit 20deb5063b2c390daccdd44405477532af02bd56

00100 routing broken on testnet (#101)
* Fixed regression introduced by 5f8552cf (00088 blank page ...).

Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

* Fixed test break in PageNotFound e2e test.

Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

src/router.ts
@@ -174,14 +174,14 @@ router.beforeEach((to, from) => {
   let result: boolean | string
 
   const toEntry = getNetworkEntryFromRoute(to)
-  const fromEntry = getNetworkEntryFromRoute(from) ?? networkRegistry.getDefaultEntry()
+  const fromEntry = getNetworkEntryFromRoute(from)
   if (toEntry !== null) {
     // Network is valid
-    if (fromEntry != toEntry) {
+    AppStorage.setLastNetwork(toEntry)
+    axios.defaults.baseURL = toEntry.url
+    if (fromEntry != null && fromEntry != toEntry) {
       // Network is changing => updates AppStorage and axios
-      AppStorage.setLastNetwork(toEntry)
-      axios.defaults.baseURL = toEntry.url
-      if (to.name != "MainDashboard") {
+      if (to.name != "MainDashboard" && to.name != "PageNotFound") {
         // We re-route on MainDashboard
         result = "/" + toEntry.name + "/dashboard"
       }