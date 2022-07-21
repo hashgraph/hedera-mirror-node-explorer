6de5a436cafe4878216e8a0524d77b55eb1da6f5
Eric Le Ponner <eric.leponner@icloud.com>
2022-07-21 15:43:58 +0200

    Added TimerUtils.timeGuard() method (to be used later).
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- src/utils/TimerUtils.ts
+++ src/utils/TimerUtils.ts
@@ -22,3 +22,17 @@
 export async function waitFor(milliseconds: number): Promise<void> {
     return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
+
+
+export async function timeGuard<T>(p: Promise<T>, milliseconds: number): Promise<T> {
+    const timeout = new Promise<T>((resolve, reject) => {
+        window.setTimeout(() => reject(new TimeGuardError()), milliseconds)
+    })
+    return Promise.race<T>([p, timeout])
+}
+
+export class TimeGuardError extends Error {
+    constructor() {
+        super("TimeGuardError")
+    }
+}
