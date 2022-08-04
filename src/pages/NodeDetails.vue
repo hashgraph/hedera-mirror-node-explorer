cd68e401174a6d4e697c2a81b2e1579ebb4f252a
Simon Viénot <simon.vienot@icloud.com>
2022-08-04 16:43:11 +0200

    Display 'Stake for Consensus' with thousands separator.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- src/pages/NodeDetails.vue
+++ src/pages/NodeDetails.vue
@@ -93,7 +93,7 @@
               <NetworkDashboardItem :name="'APPROX YEARLY EQUIVALENT'" :title="'Last Period Reward Rate'"
                                     :value="approxYearlyRate.toString()"/>
               <br/><br/>
-              <NetworkDashboardItem :name="'HBAR'" :title="'Stake for Consensus'" :value="stake.toString()"/>
+              <NetworkDashboardItem :name="'HBAR'" :title="'Stake for Consensus'" :value="stake.toLocaleString('en-US')"/>
               <p class="h-is-property-text h-is-extra-text mt-1">{{ stakePercentage }}% of total</p>
               <br/><br/>
               <NetworkDashboardItem :name="'HBAR'" :title="'Min Stake'" :value="minStake.toLocaleString('en-US')"/>
