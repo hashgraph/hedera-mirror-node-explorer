888e91c1bc9f0489a2d85cb1c8abdad85a2a3d8b
Eric Le Ponner <eric.leponner@icloud.com>
2022-08-18 14:52:06 +0200

    BlockDetails now displays some data.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- src/pages/BlockDetails.vue
+++ src/pages/BlockDetails.vue
@@ -30,6 +30,79 @@
       <template v-slot:title>
         <span class="h-is-primary-title">Block {{ blockNb ?? "" }}</span>
       </template>
+
+      <template v-slot:table>
+
+        <NotificationBanner v-if="notification" :message="notification"/>
+
+        <div class="columns h-is-property-text">
+          <div class="column">
+            <Property id="count">
+              <template v-slot:name>No. of Transactions</template>
+              <template v-slot:value>
+                <StringValue :string-value="block?.count?.toLocaleString()"/>
+              </template>
+            </Property>
+          </div>
+        </div>
+
+        <div class="columns h-is-property-text">
+          <div class="column">
+            <Property id="blockHash">
+              <template v-slot:name>Hash</template>
+              <template v-slot:value>
+                <KeyValue :key-bytes="block?.hash" key-type="SHA384" :show-none="true"/>
+              </template>
+            </Property>
+          </div>
+        </div>
+
+        <div class="columns h-is-property-text">
+          <div class="column">
+            <Property id="fromTimestamp">
+              <template v-slot:name>From Timestamp</template>
+              <template v-slot:value>
+                <TimestampValue :timestamp="block?.timestamp?.from" :show-none="true"/>
+              </template>
+            </Property>
+          </div>
+        </div>
+
+        <div class="columns h-is-property-text">
+          <div class="column">
+            <Property id="toTimestamp">
+              <template v-slot:name>To Timestamp</template>
+              <template v-slot:value>
+                <TimestampValue :timestamp="block?.timestamp?.to" :show-none="true"/>
+              </template>
+            </Property>
+          </div>
+        </div>
+
+        <div class="columns h-is-property-text">
+          <div class="column">
+            <Property id="gasUsed">
+              <template v-slot:name>Gas Used</template>
+              <template v-slot:value>
+                <StringValue :string-value="block?.gas_used?.toLocaleString()"/>
+              </template>
+            </Property>
+          </div>
+        </div>
+
+        <div class="columns h-is-property-text">
+          <div class="column">
+            <Property id="recordFileName">
+              <template v-slot:name>Record File Name</template>
+              <template v-slot:value>
+                <StringValue :string-value="block?.name"/>
+              </template>
+            </Property>
+          </div>
+        </div>
+
+      </template>
+
     </DashboardCard>
 
   </section>
@@ -44,8 +117,17 @@
 
 <script lang="ts">
 
-import {defineComponent, inject} from 'vue';
+import {computed, defineComponent, inject, onMounted, ref, watch} from 'vue';
+import {Block} from "@/schemas/HederaSchemas";
+import {BlockNb} from "@/utils/BlockNb";
 import DashboardCard from "@/components/DashboardCard.vue";
+import NotificationBanner from "@/components/NotificationBanner.vue";
+import Property from "@/components/Property.vue";
+import TimestampValue from "@/components/values/TimestampValue.vue";
+import StringValue from "@/components/values/StringValue.vue";
+import KeyValue from "@/components/values/KeyValue.vue";
+import Footer from "@/components/Footer.vue";
+import axios from "axios";
 
 export default defineComponent({
 
@@ -53,20 +135,78 @@ export default defineComponent({
 
   components: {
     DashboardCard,
+    NotificationBanner,
+    Property,
+    Footer,
+    StringValue,
+    TimestampValue,
+    KeyValue,
   },
 
   props: {
-    blockNb: Number,
+    blockNb: String,
     network: String
   },
 
-  setup() {
+  setup(props) {
     const isSmallScreen = inject('isSmallScreen', true)
     const isTouchDevice = inject('isTouchDevice', false)
+
+    const validBlockNb = computed(() => {
+      return props.blockNb ? BlockNb.parse(props.blockNb) !== null : false
+    })
+
+    //
+    // block
+    //
+
+    const block = ref<Block|null>(null)
+    const blockError = ref<unknown>(null)
+
+    const got404 = computed(() => {
+      return blockError.value !== null
+          && axios.isAxiosError(blockError.value)
+          && blockError.value?.request?.status === 404
+    })
+
+    const notification = computed(() => {
+      let result
+      if (!validBlockNb.value) {
+        result =  "Invalid block NB: " + props.blockNb
+      } else if (got404.value) {
+        result =  "Block with NB " + props.blockNb + " was not found"
+      } else {
+        result = null
+      }
+      return result
+    })
+
+    const fetchBlock = () => {
+      if (validBlockNb.value) {
+        axios
+            .get<Block>("api/v1/blocks/" + props.blockNb)
+            .then(response => {
+              block.value = response.data
+              blockError.value = null
+            })
+            .catch(reason => {
+              block.value = null
+              blockError.value = reason
+            })
+      } else {
+        block.value = null
+        blockError.value = null
+      }
+    }
+
+    watch(() => props.blockNb, () => fetchBlock())
+    onMounted(() => fetchBlock())
 
     return {
       isSmallScreen,
       isTouchDevice,
+      block,
+      notification,
     }
   }
 });
