dcd6990aecd1c536a39c7d7c9c158b1a5a554cc1
Simon Viénot <simon.vienot@icloud.com>
2022-08-24 18:51:42 +0200

    Add mention on Staking page related to Hedera staking phase.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- src/pages/Staking.vue
+++ src/pages/Staking.vue
@@ -114,6 +114,13 @@
               Your funds are fully available for use while staked.<br/>
               You can unstake or switch nodes freely.
             </div>
+            <div class="mt-5 h-is-text-size-2 is-italic has-text-grey has-text-centered">
+              <span class="has-text-grey-light">Please Note: </span>
+              Staking is in Phase 1 and will not pay out rewards until Phase 3.<br/>
+              Your full balance is automatically staked.<br/>
+              Your funds are fully available for use while staked.<br/>
+              You can unstake or switch nodes freely.
+            </div>
             <div class="mt-6"/>
           </div>
         </template>
