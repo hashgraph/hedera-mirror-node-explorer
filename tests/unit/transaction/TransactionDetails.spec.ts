80ebb8d69337f44984d819c67f45f2aa03fdcc67
Simon Viénot <simon.vienot@icloud.com>
2022-08-31 19:53:11 +0200

    Remove NetAmount per spec...
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- tests/unit/transaction/TransactionDetails.spec.ts
+++ tests/unit/transaction/TransactionDetails.spec.ts
@@ -76,7 +76,7 @@ describe("TransactionDetails.vue", () => {
         expect(wrapper.get("#transactionTypeValue").text()).toBe("CRYPTO TRANSFER")
         expect(wrapper.get("#consensusAtValue").text()).toBe("5:12:31.6676 AMFeb 28, 2022, UTC") // UTC because of HMSF.forceUTC
         expect(wrapper.get("#transactionHashValue").text()).toBe("a012 9612 32ed 7d28 4283 6e95 f7e9 c435 6fdf e2de 0819 9091 701a 969c 1d1f d936 71d3 078e e83b 28fb 460a 88b4 cbd8 ecd2Copy to Clipboard")
-        expect(wrapper.get("#netAmountValue").text()).toBe("0.00000000$0.0000")
+        // expect(wrapper.get("#netAmountValue").text()).toBe("0.00000000$0.0000")
         expect(wrapper.get("#chargedFeeValue").text()).toBe("0.00470065$0.0012")
         expect(wrapper.get("#maxFeeValue").text()).toBe("1.00000000$0.2460")
 
