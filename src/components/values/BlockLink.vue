aa0ce3916c12c6e3b8673fd6c8b5f9601c81e1a7
Simon Viénot <simon.vienot@icloud.com>
2022-09-01 12:44:24 +0200

    Add BlockLink for further use.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

---
+++ src/components/values/BlockLink.vue
@@ -0,0 +1,56 @@
+<!--
+  -
+  - Hedera Mirror Node Explorer
+  -
+  - Copyright (C) 2021 - 2022 Hedera Hashgraph, LLC
+  -
+  - Licensed under the Apache License, Version 2.0 (the "License");
+  - you may not use this file except in compliance with the License.
+  - You may obtain a copy of the License at
+  -
+  -      http://www.apache.org/licenses/LICENSE-2.0
+  -
+  - Unless required by applicable law or agreed to in writing, software
+  - distributed under the License is distributed on an "AS IS" BASIS,
+  - WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  - See the License for the specific language governing permissions and
+  - limitations under the License.
+  -
+  -->
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                     TEMPLATE                                                    -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<template>
+  <div v-if="blockNumber" class="is-inline-block">
+    <router-link :to="{name: 'BlockDetails', params: {blockHon: blockNumber}}">
+      <span class="is-numeric">{{ blockNumber }}</span>
+    </router-link>
+  </div>
+</template>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                      SCRIPT                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<script lang="ts">
+
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "BlockLink",
+
+  props: {
+    blockNumber: Number,
+  },
+});
+
+</script>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                       STYLE                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<style/>
+
