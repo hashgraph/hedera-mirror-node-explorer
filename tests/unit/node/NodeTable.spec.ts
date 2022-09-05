8cb0b1e852270dad0b5a80f0a2abc099706fdc77
Simon Viénot <simon.vienot@icloud.com>
2022-09-05 19:57:44 +0200

    Tweak tooltip for reward rate.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- tests/unit/node/NodeTable.spec.ts
+++ tests/unit/node/NodeTable.spec.ts
@@ -55,7 +55,7 @@ describe("NodeTable.vue", () => {
         " (weight is absent when the amount staked is below minimum)."
     const tooltipNotRewarded = "This is the total amount staked to this node by accounts that have chosen " +
         "to decline rewards (and all accounts staked to those accounts)."
-    const tooltipRewardRate = "This is an approximate annual reward rate based on the reward payed for the " +
+    const tooltipRewardRate = "This is an approximate annual reward rate based on the reward earned during the " +
         "last 24h period."
 
     it("should list the 3 nodes in the table", async () => {
