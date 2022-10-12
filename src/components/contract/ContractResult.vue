4a321b7a3336aba912281a8a59252a35a044d447
Simon Viénot <simon.vienot@icloud.com>
2022-10-12 20:35:53 +0200

    Filter out ContractResultStates card for now.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- src/components/contract/ContractResult.vue
+++ src/components/contract/ContractResult.vue
@@ -110,7 +110,7 @@
 
     <ContractResultTrace :transaction-id-or-hash="transactionIdOrHash"/>
 
-    <ContractResultStates :state-changes="contractResult?.state_changes" />
+    <ContractResultStates v-if="false" :state-changes="contractResult?.state_changes" />
 
     <DashboardCard v-if="contractResult?.logs?.length" class="h-card">
       <template v-slot:title>
