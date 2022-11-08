d2e647b0708c1626d7a262a5065bfb68f5dd324e
Simon Viénot <simon.vienot@icloud.com>
2022-11-08 15:34:38 +0100

    Initial support for HTML meta tags.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- src/router.ts
+++ src/router.ts
@@ -286,39 +286,47 @@ router.beforeEach((to, from) => {
 })
 
 router.beforeEach((to) => {
-  const titlePrefix = process.env.VUE_APP_DOCUMENT_TITLE_PREFIX ?? "Hedera"
+  const titleSuffix = process.env.VUE_APP_DOCUMENT_TITLE_SUFFIX ? " | " + process.env.VUE_APP_DOCUMENT_TITLE_SUFFIX : ""
 
   switch (to.name as string) {
     case "MainDashboard":
-      document.title = titlePrefix + " | Dashboard";
+      document.title = "Hedera Dashboard" + titleSuffix
       break;
     case "TransactionDetails":
-      document.title = titlePrefix + " | Transaction " + to.params.transactionId;
+      document.title = "Hedera Transaction " + to.params.transactionId + titleSuffix
       break;
     case "TokenDetails":
-      document.title = titlePrefix + " | Token " + to.params.tokenId;
+      document.title = "Hedera Token " + to.params.tokenId + titleSuffix
       break;
     case "TopicDetails":
-      document.title = titlePrefix + " | Topic " + to.params.topicId;
+      document.title = "Hedera Topic " + to.params.topicId + titleSuffix
       break;
     case "ContractDetails":
-      document.title = titlePrefix + " | Contract " + to.params.contractId;
+      document.title = "Hedera Contract " + to.params.contractId + titleSuffix
       break;
     case "AccountDetails":
-      document.title = titlePrefix + " | Account " + to.params.accountId;
+      document.title = "Hedera Account " + to.params.accountId + titleSuffix
       break;
     case "AccountBalances":
-      document.title = titlePrefix + " | Balances for Account " + to.params.accountId;
+      document.title = "Balances for Hedera Account " + to.params.accountId + titleSuffix
+      break;
+    case "NodeDetails":
+      document.title = "Hedera Node " + to.params.nodeId + titleSuffix
+      break;
+    case "BlockDetails":
+      document.title = "Hedera Block " + to.params.blockHon + titleSuffix
       break;
     case "NoSearchResult":
-      document.title = titlePrefix + " | Search Results";
+      document.title = "Search Results" + titleSuffix
       break;
     case "PageNotFound":
-      document.title = titlePrefix + " | Page Not Found";
+      document.title = "Page Not Found" + titleSuffix
       break;
     default:
-      document.title = titlePrefix + " | " + (to.name as string);
+      document.title = "Hedera " + (to.name as string) + titleSuffix
   }
+
+  addMetaTags()
 });
 
 router.beforeEach(() => {
@@ -326,6 +334,33 @@ router.beforeEach(() => {
 })
 
 export default router
+
+export function addMetaTags(): void {
+
+  const title = document.title
+  const description = process.env.VUE_APP_META_DESCRIPTION
+      ?? "Hedera Mirror Node Explorer is a ledger explorer for the Hedera network"
+  const url = process.env.VUE_APP_META_URL
+
+  const header = document.getElementsByTagName('head')[0]
+
+  const metaDescription = document.createElement('meta');
+  metaDescription.name = 'description';
+  metaDescription.setAttribute('content', description);
+  header.appendChild(metaDescription);
+
+  const metaTitle = document.createElement('meta');
+  metaTitle.setAttribute('property', 'og:title')
+  metaTitle.setAttribute('content', title)
+  header.appendChild(metaTitle);
+
+  if (url) {
+    const metaUrl = document.createElement('meta');
+    metaUrl.setAttribute('property', 'og:url')
+    metaUrl.setAttribute('content', url)
+    header.appendChild(metaUrl);
+  }
+}
 
 export function getNetworkEntryFromRoute(r: RouteLocationNormalized): NetworkEntry | null {
 
