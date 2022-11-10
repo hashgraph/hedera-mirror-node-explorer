93d6e240ca2f481685640edea7265b6ec6b2bf92
Simon Viénot <simon.vienot@icloud.com>
2022-11-10 19:44:35 +0100

    Adjust units tests to the presence of extra dialogs in AccountDetails view.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- tests/unit/account/AccountDetails.spec.ts
+++ tests/unit/account/AccountDetails.spec.ts
@@ -114,7 +114,7 @@ describe("AccountDetails.vue", () => {
         await flushPromises()
         // console.log(wrapper.html())
 
-        expect(wrapper.text()).toMatch(RegExp("^Account " + SAMPLE_ACCOUNT.account))
+        expect(wrapper.text()).toMatch("Account " + SAMPLE_ACCOUNT.account)
         expect(wrapper.get("#balanceValue").text()).toBe("23.42647909$5.7637998234231ĦFRENSKINGDOM")
         expect(wrapper.get("#keyValue").text()).toBe(
             "aa2f 7b3e 759f 4531 ec2e 7941 afa4 49e6 a6e6 10ef b52a dae8 9e9c d8e9 d40d dcbf" +
@@ -172,7 +172,7 @@ describe("AccountDetails.vue", () => {
         // console.log(wrapper.html())
         // console.log(wrapper.text())
 
-        expect(wrapper.text()).toMatch(RegExp("^Account " + SAMPLE_ACCOUNT.account))
+        expect(wrapper.text()).toMatch("Account " + SAMPLE_ACCOUNT.account)
         expect(wrapper.get("#keyValue").text()).toBe(
             "aa2f 7b3e 759f 4531 ec2e 7941 afa4 49e6 a6e6 10ef b52a dae8 9e9c d8e9 d40d dcbf" +
             "Copy to Clipboard" +
@@ -193,7 +193,7 @@ describe("AccountDetails.vue", () => {
         // console.log(wrapper.html())
         // console.log(wrapper.text())
 
-        expect(wrapper.text()).toMatch(RegExp("^Account " + SAMPLE_ACCOUNT_DUDE.account))
+        expect(wrapper.text()).toMatch("Account " + SAMPLE_ACCOUNT_DUDE.account)
         expect(wrapper.get("#keyValue").text()).toBe(
             "38f1 ea46 0e95 d97e ea13 aefa c760 eaf9 9015 4b80 a360 8ab0 1d4a 2649 44d6 8746" +
             "Copy to Clipboard" +
@@ -270,7 +270,7 @@ describe("AccountDetails.vue", () => {
         await flushPromises()
         // console.log(wrapper.text())
 
-            expect(wrapper.text()).toMatch(RegExp("^Account " + deletedAccount.account))
+            expect(wrapper.text()).toMatch(RegExp("Account " + deletedAccount.account))
 
         const banner = wrapper.findComponent(NotificationBanner)
         expect(banner.exists()).toBe(true)
