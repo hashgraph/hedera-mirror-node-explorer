cfdce617665feefabde329d18ecfee19ba8c9cfe
Simon Viénot <simon.vienot@icloud.com>
2022-12-21 18:30:55 +0100

    Display associated token in TransactionDetails.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- src/pages/TransactionDetails.vue
+++ src/pages/TransactionDetails.vue
@@ -99,6 +99,12 @@
       </template>
 
       <template v-slot:rightContent>
+        <Property v-if="isTokenAssociation" id="associatedTokenId">
+          <template v-slot:name>Associated Token</template>
+          <template v-slot:value>
+            <TokenLink :token-id="associatedToken" :show-extra="true"/>
+          </template>
+        </Property>
         <Property v-if="systemContract" id="entityId">
           <template v-slot:name>Contract ID</template>
           <template v-slot:value>{{ systemContract }}</template>
@@ -252,6 +258,7 @@ import {routeManager} from "@/router"
 import {Timestamp} from "@/utils/Timestamp";
 import {TransactionHash} from "@/utils/TransactionHash";
 import {TokenRelationshipLoader} from "@/components/token/TokenRelationshipLoader";
+import TokenLink from "@/components/values/TokenLink.vue";
 
 const MAX_INLINE_CHILDREN = 9
 
@@ -260,6 +267,7 @@ export default defineComponent({
   name: 'TransactionDetails',
 
   components: {
+    TokenLink,
     TopicMessage,
     ContractResult,
     BlockLink,
@@ -331,10 +339,11 @@ export default defineComponent({
     )
     const topicMessageLoader = new TopicMessageLoader(messageTimestamp)
 
-    const associatedAccount = computed(() =>
-        (transactionLoader.transactionType.value === TransactionType.TOKENASSOCIATE)
-            ? transactionLoader.entity.value?.entity_id ?? null
-            : null
+    const isTokenAssociation = computed(
+        () => transactionLoader.transactionType.value === TransactionType.TOKENASSOCIATE)
+
+    const associatedAccount = computed(
+        () => isTokenAssociation.value ? transactionLoader.entity.value?.entity_id ?? null : null
     )
     const tokenRelationships = new TokenRelationshipLoader(associatedAccount)
 
@@ -371,6 +380,7 @@ export default defineComponent({
       routeToAllTransactions,
       displayAllChildrenLinks,
       topicMessageLoader,
+      isTokenAssociation,
       associatedToken
     }
   },
