4b8789c0a19ef4f5ee095d91658f655deb3189f0
Simon Viénot <simon.vienot@icloud.com>
2023-01-04 17:59:21 +0100

    00384 HbarAmount should display None when amount is null. (#386)
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- tests/unit/transaction/TransactionDetails.spec.ts
+++ tests/unit/transaction/TransactionDetails.spec.ts
@@ -195,7 +195,7 @@ describe("TransactionDetails.vue", () => {
         expect(wrapper.get("#gasUsedValue").text()).toBe("384,000")
         expect(wrapper.get("#maxFeePerGasValue").text()).toBe("None")
         expect(wrapper.get("#maxPriorityFeePerGasValue").text()).toBe("None")
-        expect(wrapper.get("#gasPriceValue").text()).toBe("0.00000000$0.0000")
+        expect(wrapper.get("#gasPriceValue").text()).toBe("None")
         expect(wrapper.findAll("#logIndexValue").length).toBe(3)
     });
 
@@ -255,7 +255,7 @@ describe("TransactionDetails.vue", () => {
         expect(wrapper.get("#gasUsedValue").text()).toBe("384,000")
         expect(wrapper.get("#maxFeePerGasValue").text()).toBe("None")
         expect(wrapper.get("#maxPriorityFeePerGasValue").text()).toBe("None")
-        expect(wrapper.get("#gasPriceValue").text()).toBe("0.00000000$0.0000")
+        expect(wrapper.get("#gasPriceValue").text()).toBe("None")
         expect(wrapper.findAll("#logIndexValue").length).toBe(3)
     });
 
