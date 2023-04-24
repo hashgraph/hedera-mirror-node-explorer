de332a6dd9e846f39b25d17b5a94ff8617c178f0
Eric Le Ponner <eric.leponner@icloud.com>
2023-04-24 17:26:03 +0200

    Added AssetCache (to be used later).
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

---
+++ src/utils/cache/AssetCache.ts
@@ -0,0 +1,37 @@
+/*-
+ *
+ * Hedera Mirror Node Explorer
+ *
+ * Copyright (C) 2021 - 2022 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+import axios from "axios";
+import {EntityCache} from "@/utils/cache/base/EntityCache";
+
+export class AssetCache extends EntityCache<string, unknown> {
+
+    public static readonly instance = new AssetCache()
+
+    //
+    // Cache
+    //
+
+    protected async load(url: string): Promise<unknown> {
+        const response = await axios.get<unknown>(url)
+        return Promise.resolve(response.data)
+    }
+
+}
