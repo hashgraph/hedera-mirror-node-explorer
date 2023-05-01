a3211b5b8cdc9683a98aa2f483050a322726b577
Eric Le Ponner <eric.leponner@icloud.com>
2023-05-01 12:37:08 +0200

    Moved 'matchMedia' mock up instructions in globalSetup.js.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- tests/unit/TopNavBar.spec.ts
+++ tests/unit/TopNavBar.spec.ts
@@ -32,20 +32,6 @@ import {HMSF} from "@/utils/HMSF";
 
  */
 
-Object.defineProperty(window, 'matchMedia', {
-    writable: true,
-    value: jest.fn().mockImplementation(query => ({
-        matches: false,
-        media: query,
-        onchange: null,
-        addListener: jest.fn(), // deprecated
-        removeListener: jest.fn(), // deprecated
-        addEventListener: jest.fn(),
-        removeEventListener: jest.fn(),
-        dispatchEvent: jest.fn(),
-    })),
-});
-
 HMSF.forceUTC = true
 
 describe("TopNavBar.vue", () => {
--- tests/unit/contract/ContractDetails.spec.ts
+++ tests/unit/contract/ContractDetails.spec.ts
@@ -47,20 +47,6 @@ import ContractResultTable from "@/components/contract/ContractResultTable.vue";
 
  */
 
-Object.defineProperty(window, 'matchMedia', {
-    writable: true,
-    value: jest.fn().mockImplementation(query => ({
-        matches: false,
-        media: query,
-        onchange: null,
-        addListener: jest.fn(), // deprecated
-        removeListener: jest.fn(), // deprecated
-        addEventListener: jest.fn(),
-        removeEventListener: jest.fn(),
-        dispatchEvent: jest.fn(),
-    })),
-});
-
 HMSF.forceUTC = true
 
 describe("ContractDetails.vue", () => {
