1b52469f97e23d1e930f844b658f997927196b22
Simon Viénot <simon.vienot@icloud.com>
2023-05-31 11:22:43 +0200

    Use InfoTooltip in AccountDetails.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- tests/unit/node/NodeDetails.spec.ts
+++ tests/unit/node/NodeDetails.spec.ts
@@ -182,7 +182,7 @@ describe("NodeDetails.vue", () => {
         expect(wrapper.get("#memoValue").text()).toBe("Account Dude Memo in clear")
         expect(wrapper.find("#aliasValue").exists()).toBe(false)
         expect(wrapper.get("#expiresAtValue").text()).toBe("3:33:21.4109 AMApr 11, 2022, UTC")
-        expect(wrapper.get("#autoRenewPeriodValue").text()).toBe("Not yet enabled")
+        expect(wrapper.get("#autoRenewPeriodValue").text()).toBe("77d 3h 40min")
         expect(wrapper.get("#maxAutoAssociationValue").text()).toBe("10")
         expect(wrapper.get("#receiverSigRequiredValue").text()).toBe("true")
 
