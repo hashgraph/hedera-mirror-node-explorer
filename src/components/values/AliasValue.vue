ecd047e0e9115148d9c730c71ea878b1b5b50bfd
Simon Viénot <simon.vienot@icloud.com>
2023-06-08 17:41:35 +0200

    Use Copyable component in AliasValue.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- src/components/values/AliasValue.vue
+++ src/components/values/AliasValue.vue
@@ -26,16 +26,11 @@
   <div v-if="hexValue" class="should-wrap">
     <div :class="{'is-flex': isSmallScreen}" class="is-inline-block h-is-text-size-3 is-family-monospace"
          style="line-height: 20px">
-      <div class="shy-scope mr-1" style="display: inline-block; position: relative;">
-        <span class="has-text-grey">0x</span>
-        <span>{{ hexValue }}</span>
-        <div id="shyCopyButton" class="shy" style="position: absolute; left: 0; top: 0; width: 100%; height: 100%">
-          <div style="position: absolute; left: 0; top: 0; width: 100%; height: 100%; background: rgba(0, 0, 0, 0.50)"></div>
-          <div style="position: absolute; display: inline-block; left: 50%; top: 50%; transform: translate(-50%, -50%);">
-            <button class="button is-dark h-is-text-size-3" @click.stop="copyToClipboard">Copy to Clipboard</button>
-          </div>
-        </div>
-      </div>
+      <Copyable :content-to-copy="hexValue">
+        <template v-slot:content>
+          <span>{{ hexValue }}</span>
+        </template>
+      </Copyable>
     </div>
   </div>
   <div v-else-if="initialLoading"/>
@@ -51,9 +46,11 @@
 import {computed, defineComponent, inject, ref} from "vue";
 import {base32ToAlias, byteToHex} from "@/utils/B64Utils";
 import {initialLoadingKey} from "@/AppKeys";
+import Copyable from "@/components/Copyable.vue";
 
 export default defineComponent({
   name: "AliasValue",
+  components: {Copyable},
   props: {
     aliasValue: String,
   },
@@ -72,17 +69,10 @@ export default defineComponent({
       return result
     })
 
-    const copyToClipboard = (): void => {
-      if (hexValue.value) {
-        navigator.clipboard.writeText(hexValue.value)
-      }
-    }
-
     return {
       initialLoading,
       isSmallScreen,
       hexValue,
-      copyToClipboard
     }
   }
 })
@@ -93,14 +83,4 @@ export default defineComponent({
 <!--                                                      STYLE                                                      -->
 <!-- --------------------------------------------------------------------------------------------------------------- -->
 
-<style scoped>
-
-.shy {
-  display: none
-}
-
-.shy-scope:hover > .shy {
-  display: block;
-}
-
-</style>
+<style/>
