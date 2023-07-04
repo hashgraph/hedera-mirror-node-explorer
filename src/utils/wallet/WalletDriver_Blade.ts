83cd2b2d3a0166f05566201cd3f71f9aa1b15cde
Eric Le Ponner <eric.leponner@icloud.com>
2023-07-04 11:32:43 +0200

    Fixed typing issues in WalletDriver class and subclasses.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- src/utils/wallet/WalletDriver_Blade.ts
+++ src/utils/wallet/WalletDriver_Blade.ts
@@ -23,7 +23,7 @@ import {BladeSigner, BladeWalletError} from "@bladelabs/blade-web3.js";
 import {HederaNetwork} from "@bladelabs/blade-web3.js/lib/src/models/blade";
 import {WalletDriver} from "@/utils/wallet/WalletDriver";
 import {WalletDriverError} from "@/utils/wallet/WalletDriverError";
-import {Signer} from "@hashgraph/sdk/lib/Signer";
+import {Signer} from "@hashgraph/sdk";
 
 export class WalletDriver_Blade extends WalletDriver {
 
