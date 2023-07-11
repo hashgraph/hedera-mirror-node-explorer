64e59e1ce21041805e5bc0e83b6f2b89e4c3e0b0
Eric Le Ponner <eric.leponner@icloud.com>
2023-07-11 19:22:33 +0200

    Configured vite-plugin-rewrite-all in dev server to avoid 404 on dotted urls.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import basicSsl from '@vitejs/plugin-basic-ssl'
+import pluginRewriteAll from 'vite-plugin-rewrite-all'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    basicSsl()
+    basicSsl(),
+    pluginRewriteAll() // See vite issue: https://github.com/vitejs/vite/issues/2190
   ],
   resolve: {
     alias: {
