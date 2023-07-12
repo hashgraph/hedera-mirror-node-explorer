92bbd81f050c43c02022e65b1ee92728b31d0353
Eric Le Ponner <eric.leponner@icloud.com>
2023-07-12 16:37:26 +0200

    Migration to vite: added polyfill for NodeJS buffer.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,29 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import pluginRewriteAll from 'vite-plugin-rewrite-all'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     basicSsl(),
-    pluginRewriteAll() // See vite issue: https://github.com/vitejs/vite/issues/2190
+    pluginRewriteAll(), // See vite issue: https://github.com/vitejs/vite/issues/2190
+    nodePolyfills({ // hashgraph/proto needs NodeJS buffers
+      exclude: [
+        'fs', // Excludes the polyfill for `fs` and `node:fs`.
+      ],
+      globals: {
+        Buffer: true,
+        global: false,
+        process: false,
+      },
+      protocolImports: false,
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
 })
