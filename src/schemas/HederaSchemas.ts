7819f93f383a2c2f1c0ad99dfdfba18d8e451b7e
Simon Viénot <simon.vienot@icloud.com>
2023-10-23 15:57:55 +0200

    Complete definition of NetworkStake interface.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- src/schemas/HederaSchemas.ts
+++ src/schemas/HederaSchemas.ts
@@ -690,16 +690,26 @@ export interface NetworkSupplyResponse {
     total_supply: string | undefined  // The network's total supply of hbars in tinybars
 }
 
-export interface  NetworkStake {
-    max_staking_reward_rate_per_hbar: number,
-    node_reward_fee_fraction: number,
-    stake_total: number,
-    staking_period: TimestampRange,
-    staking_period_duration: number,
-    staking_periods_stored: number,
-    staking_reward_fee_fraction: number,
-    staking_reward_rate: number,
-    staking_start_threshold: number
+export interface NetworkStake {
+    max_stake_rewarded: number,               // The maximum amount of tinybar that can be staked for reward while still
+                                              // achieving the maximum per-hbar reward rate
+    max_staking_reward_rate_per_hbar: number, // The maximum reward rate, in tinybars per whole hbar, that any account can receive in a day
+    max_total_reward: number,                 // The total tinybars to be paid as staking rewards in the ending period,
+                                              // after applying the settings for the 0.0.800 balance threshold and the maximum stake rewarded
+    node_reward_fee_fraction: number,         // The fraction between zero and one of the network and service fees paid to the node reward account 0.0.801
+    reserved_staking_rewards: number,         // The amount of the staking reward funds of account 0.0.800 reserved to
+                                              // pay pending rewards that have been earned but not collected
+    reward_balance_threshold: number,         // The unreserved tinybar balance of account 0.0.800 required to achieve the maximum per-hbar reward rate
+    stake_total: number,                      // The total amount staked to the network in tinybars the start of the current staking period
+    staking_period: TimestampRange,           // The timestamp range of the staking period
+    staking_period_duration: number,          // The number of minutes in a staking period
+    staking_periods_stored: number,           // The number of staking periods for which the reward is stored for each node
+    staking_reward_fee_fraction: number,      // The fraction between zero and one of the network and service fees paid
+                                              // to the staking reward account 0.0.800
+    staking_reward_rate: number,              // The total number of tinybars to be distributed as staking rewards each period
+    staking_start_threshold: number,          // The minimum balance of staking reward account 0.0.800 required to active rewards
+    unreserved_staking_reward_balance: number // The unreserved balance of account 0.0.800 at the close of the just-ending period;
+                                              // this value is used to compute the HIP-782 balance ratio
 }
 
 // ---------------------------------------------------------------------------------------------------------------------
