f9e61851aa99233753f12bcce3f5868959648c6c
Simon Viénot <simon.vienot@icloud.com>
2023-10-24 17:26:03 +0200

    Drop ability to remove elements from table in FileList.vue
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- src/components/verification/FileList.vue
+++ src/components/verification/FileList.vue
@@ -25,10 +25,10 @@
 <!--suppress CssUnusedSymbol -->
 
 <template>
-    <div v-if="contractFileList" id="file-table">
+    <div v-if="fileList" id="file-table">
         <o-table
             :current-page="currentPage"
-            :data="contractFileList"
+            :data="fileList"
             :paginated="isPaginated"
             :per-page="perPage"
             aria-current-label="Current page"
@@ -38,32 +38,17 @@
 
             <o-table-column v-slot="props" :label="tableTitle" field="type_and_name">
                 <div class="is-flex is-align-items-center">
-                    <img v-if="props.row.type === 'json'" alt="JSON file" class="image"
+                    <img v-if="isMetadata(props.row)" alt="JSON file" class="image"
                          src="../../assets/json-file.svg" style="width: 20px; height: 20px">
-                    <img v-else-if="props.row.type === 'sol'" alt="Solidity file" class="image"
+                    <img v-else alt="Solidity file" class="image"
                          src="../../assets/solidity-icon.svg" style="width: 20px; height: 20px">
-                    <i v-else class="fas fa-question"></i>
-                    <p :class="{'has-text-grey': props.row.removed}" class="ml-2">
-                        {{ props.row.name }}
-                    </p>
-                </div>
-            </o-table-column>
-
-            <o-table-column v-slot="props" field="remove_action" label="" position="right">
-                <div v-if="!props.row.removed" class="is-flex is-align-items-center is-align-content-flex-end"
-                     style="width: 20px">
-                    <img alt="Trash" class="image"
-                         src="@/assets/red-trash.svg"
-                         style="width: 20px; height: 20px" @click="handleRemove(props.index)">
-                    <p class="ml-1" style="color: #ED6A5E"
-                       @click="handleRemove(props.index)">
-                        Remove
+                    <p class="ml-2">
+                        {{ props.row }}
                     </p>
                 </div>
             </o-table-column>
 
         </o-table>
-
     </div>
 </template>
 
@@ -73,13 +58,7 @@
 
 <script lang="ts">
 
-import {computed, defineComponent, inject, onMounted, PropType, ref, watch} from "vue";
-
-export interface ContractFile {
-    type: string,
-    name: string,
-    removed: boolean
-}
+import {computed, defineComponent, inject, PropType, ref} from "vue";
 
 export default defineComponent({
     name: 'FileList',
@@ -93,9 +72,7 @@ export default defineComponent({
         }
     },
 
-    emits: ["update:fileList"],
-
-    setup: function (props, context) {
+    setup: function (props) {
         const isTouchDevice = inject('isTouchDevice', false)
         const isSmallScreen = inject('isSmallScreen', true)
         const isMediumScreen = inject('isMediumScreen', true)
@@ -103,32 +80,12 @@ export default defineComponent({
         const currentPage = ref(1);
         const perPage = ref(10);
         const isPaginated = computed(() => props.fileList.length > perPage.value)
-        const tableTitle = computed(() => `Added Files (${contractFileList.value.length})`)
+        const tableTitle = computed(() => `Added Files (${props.fileList.length})`)
 
-        const contractFileList = ref<ContractFile[]>([])
-        onMounted(() => upDateContractFileList())
-        watch(() => props.fileList, () => upDateContractFileList())
-        const upDateContractFileList = () => {
-            const result: Array<ContractFile> = []
-            props.fileList.forEach((file) => {
-                    const parts = file.split('.')
-                    const suffix = parts[parts.length - 1].toLowerCase()
-                    result.push({
-                        type: suffix,
-                        name: file.toString(),
-                        removed: false
-                    })
-                }
-            )
-            contractFileList.value = result
-        }
-
-        const handleRemove = (index: number) => {
-            const result = Array.from(props.fileList)
-            const globalIndex = perPage.value * (currentPage.value - 1) + index
-            result.splice(index, 1)
-            context.emit('update:fileList', result)
-            contractFileList.value[globalIndex].removed = true
+        const isMetadata = (fileName: string) => {
+            const parts = fileName.split('.')
+            const suffix = parts[parts.length - 1].toLowerCase()
+            return suffix.toLowerCase() === 'json'
         }
 
         return {
@@ -139,8 +96,7 @@ export default defineComponent({
             currentPage,
             perPage,
             tableTitle,
-            contractFileList,
-            handleRemove
+            isMetadata
         }
     }
 });
@@ -158,13 +114,12 @@ export default defineComponent({
     padding-top: 0;
     padding-left: 0;
     padding-bottom: 12px;
-}
-
-#file-table th.o-table__th {
     border-width: 0;
 }
 
 #file-table td.o-table__td {
     border-width: 0;
+    padding-top: 0.25rem;
+    padding-bottom: 0.25rem;
 }
 </style>
