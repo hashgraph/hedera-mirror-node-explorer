c7dffa34b2da303b90b2bd1fb6f2cf33f75d05e7
Simon Viénot <simon.vienot@icloud.com>
2023-11-28 17:42:05 +0100

    00694 display both raw and b64-decoded values of memo when relevant (#775)
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- src/components/values/BlobValue.vue
+++ src/components/values/BlobValue.vue
@@ -33,7 +33,19 @@
          :style="{'max-width': windowWidth-limitingFactor + 'px'}">{{ decodedValue }}</div>
     <div v-else-if="limitingFactor" class="h-is-one-line is-inline-block"
          :style="{'max-width': windowWidth-limitingFactor+200 + 'px'}">{{ decodedValue }}</div>
-    <div v-else style="word-break: break-word">{{ decodedValue }}</div>
+    <div v-else style="word-break: break-word">
+      <span id="blob-main">
+        {{ (b64EncodingFound && showBase64AsExtra) ? blobValue : decodedValue }}
+      </span>
+      <div v-if="b64EncodingFound && showBase64AsExtra" class="h-is-extra-text h-is-text-size-3 mt-1">
+        <span class="has-text-grey">
+          Base64:
+        </span>
+        <span id="blob-extra">
+          {{decodedValue }}
+        </span>
+      </div>
+    </div>
   </template>
   <span v-else-if="showNone && !initialLoading" class="has-text-grey">None</span>
   <span v-else/>
@@ -61,6 +73,10 @@ export default defineComponent({
       default: false
     },
     base64: {
+      type: Boolean,
+      default: false
+    },
+    showBase64AsExtra: {
       type: Boolean,
       default: false
     },
@@ -114,15 +130,19 @@ export default defineComponent({
       return result
     })
 
+    const b64EncodingFound = ref(false)
+
     const decodedValue = computed(() => {
 
       const base64regex = /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
-
       let result: string
+      b64EncodingFound.value = false
+
       if (props.blobValue) {
         if (props.base64 && base64regex.test(props.blobValue)) {
           try {
             result = Buffer.from(props.blobValue, 'base64').toString()
+            b64EncodingFound.value = true
           } catch {
             result = props.blobValue
           }
@@ -149,6 +169,7 @@ export default defineComponent({
       windowWidth,
       isURL,
       jsonValue,
+      b64EncodingFound,
       decodedValue,
       initialLoading,
       ipfsAddress,
