bb1ce29f638dbbadb22390817dc8f4311720dca3
Simon Viénot <simon.vienot@icloud.com>
2024-04-22 18:07:10 +0200

    Adjust ArrowSegment to spacing changes introduced by EntityIdOrLabel.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- src/components/transfer_graphs/ArrowSegment.vue
+++ src/components/transfer_graphs/ArrowSegment.vue
@@ -83,7 +83,7 @@ export default defineComponent({
     const updateSvgContent = () => {
       if (parentElement.value != null) {
         const bb = parentElement.value.getBoundingClientRect();
-        const dy = props.compact ? 7 : 9.5
+        const dy = props.compact ? 11.5 : 13
         svgContent.value = makeTransferSVG(bb.width, bb.height, dy, props.sourceCount, props.destCount, props.rowIndex)
       }
     }
