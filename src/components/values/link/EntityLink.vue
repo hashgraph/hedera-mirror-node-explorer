e3c4d728c62340bc693f970e07e32dc0500f3e5f
Eric Le Ponner <eric.leponner@icloud.com>
2024-04-25 18:01:01 +0200

    Added new EntityLink vue (to be used later).
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

---
+++ src/components/values/link/EntityLink.vue
@@ -0,0 +1,77 @@
+<!--
+  -
+  - Hedera Mirror Node Explorer
+  -
+  - Copyright (C) 2021 - 2024 Hedera Hashgraph, LLC
+  -
+  - Licensed under the Apache License, Version 2.0 (the "License");
+  - you may not use this file except in compliance with the License.
+  - You may obtain a copy of the License at
+  -
+  -      http://www.apache.org/licenses/LICENSE-2.0
+  -
+  - Unless required by applicable law or agreed to in writing, software
+  - distributed under the License is distributed on an "AS IS" BASIS,
+  - WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  - See the License for the specific language governing permissions and
+  - limitations under the License.
+  -
+  -->
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                     TEMPLATE                                                    -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<template>
+
+  <div class="is-inline-block">
+
+    <template v-if="route === null">
+      <slot/>
+    </template>
+
+    <template v-else>
+      <router-link :to="route">
+        <span class="h-is-hoverable">
+          <slot/>
+        </span>
+      </router-link>
+    </template>
+
+  </div>
+
+</template>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                      SCRIPT                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<script lang="ts">
+
+import {defineComponent, PropType} from "vue";
+import AccountLink from "@/components/values/link/AccountLink.vue";
+import TokenLink from "@/components/values/link/TokenLink.vue";
+import TopicLink from "@/components/values/link/TopicLink.vue";
+import ContractLink from "@/components/values/link/ContractLink.vue";
+import AccountIOL from "@/components/values/link/AccountIOL.vue";
+import {RouteLocationRaw} from "vue-router";
+
+export default defineComponent({
+  name: "EntityLink",
+  components: {},
+  props: {
+    route: {
+      type: Object as PropType<RouteLocationRaw|null>,
+      default: null
+    },
+  },
+});
+
+</script>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                       STYLE                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<style/>
+
