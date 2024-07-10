4c4fa9b0c52062442456ef2e2024dedfa1b902a1
Simon Viénot <simon.vienot@icloud.com>
2024-07-10 12:21:26 +0200

    Add TransactionId component (for further use).
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

---
+++ src/components/values/TransactionId.vue
@@ -0,0 +1,68 @@
+<!--
+  -
+  - Hedera Mirror Node Explorer
+  -
+  - Copyright (C) 2021 - 2024 Hedera Hashgraph, LLC
+  -
+  - Licensed under the Apache License, Version 2.0 (the "License");
+  - you may not use this file except in compliance with the License.
+  - You may obtain a copy of the License at
+  -
+  -      http://www.apache.org/licenses/LICENSE-2.0
+  -
+  - Unless required by applicable law or agreed to in writing, software
+  - distributed under the License is distributed on an "AS IS" BASIS,
+  - WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  - See the License for the specific language governing permissions and
+  - limitations under the License.
+  -
+  -->
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                     TEMPLATE                                                    -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<template>
+
+  <div>
+    <span>{{ transactionId }}</span>
+  </div>
+
+</template>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                      SCRIPT                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<script lang="ts">
+
+import {defineComponent, PropType, ref, watch} from "vue";
+import {TransactionID} from "@/utils/TransactionID";
+
+export default defineComponent({
+  name: "TransactionId",
+  props: {
+    id: {
+      type: String as PropType<string | null>,
+      default: null
+    }
+  },
+  setup(props) {
+    const transactionId = ref(TransactionID.normalize(props.id ?? ''))
+    watch([() => props.id, TransactionID.useArobasForm], () =>
+            transactionId.value = TransactionID.normalize(props.id ?? ''), {immediate: true}
+    )
+    return {
+      transactionId
+    }
+  }
+});
+
+</script>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                       STYLE                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<style/>
+
