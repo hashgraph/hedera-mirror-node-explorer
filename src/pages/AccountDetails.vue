107b21c5c70d1d546a06f9aaec4d9a8ee094fa35
Simon Viénot <simon.vienot@icloud.com>
2024-09-11 10:11:07 +0200

    Add UpdateAccountDialog in AccountDetails.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- src/pages/AccountDetails.vue
+++ src/pages/AccountDetails.vue
@@ -327,6 +327,8 @@
 
   <Footer/>
 
+  <UpdateAccountDialog :controller="updateDialogController"/>
+
 </template>
 
 <!-- --------------------------------------------------------------------------------------------------------------- -->
@@ -384,6 +386,7 @@ import InfoTooltip from "@/components/InfoTooltip.vue";
 import {labelForAutomaticTokenAssociation} from "@/schemas/HederaUtils";
 import TokensSection from "@/components/token/TokensSection.vue";
 import EditableProperty from "@/components/EditableProperty.vue";
+import UpdateAccountDialog from "@/components/account/UpdateAccountDialog.vue";
 
 const props = defineProps({
   accountId: String,
@@ -533,7 +536,13 @@ const nameQuery = new NameQuery(computed(() => props.accountId ?? null))
 onMounted(() => nameQuery.mount())
 onBeforeUnmount(() => nameQuery.unmount())
 
-const onUpdateAccount = () => alert('NOT IMPLEMENTED')
+//
+// Account Update
+//
+
+const updateDialogController = new DialogController()
+
+const onUpdateAccount = () => updateDialogController.visible.value = true
 
 const isWalletConnected = computed(() => walletManager.connected.value && walletManager.accountId.value === props.accountId)
 const isHederaWallet = computed(() => walletManager.isHederaWallet.value)
