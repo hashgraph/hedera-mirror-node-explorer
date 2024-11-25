f38535d03587e6e68095e5d13b1c72d7b1757c48
Eric Le Ponner <eric.leponner@icloud.com>
2024-11-25 16:54:49 +0100

    Moved AccountERC20Cache.getBalance() to ERCUtils.loadBalance().
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- src/utils/cache/AccountERC20Cache.ts
+++ src/utils/cache/AccountERC20Cache.ts
@@ -18,14 +18,12 @@
  *
  */
 
-import {ContractCallRequest, ContractCallResponse} from "@/schemas/MirrorNodeSchemas.ts";
 import {EntityCache} from "@/utils/cache/base/EntityCache";
-import axios from "axios";
 import {ERC20Cache} from "@/utils/cache/ERC20Cache";
 import {ERC20Info, ERC20InfoCache} from "@/utils/cache/ERC20InfoCache.ts";
 import {ContractByIdCache} from "@/utils/cache/ContractByIdCache";
-import {ethers} from "ethers";
 import {AccountByIdCache} from "@/utils/cache/AccountByIdCache";
+import {ERCUtils} from "@/utils/ERCUtils.ts";
 
 export class AccountERC20Cache extends EntityCache<string, AccountERC20[]> {
 
@@ -39,19 +37,31 @@ export class AccountERC20Cache extends EntityCache<string, AccountERC20[]> {
         if (this.contains(accountId)) {
             let newPromise: Promise<AccountERC20[]>
             try {
-                const accountERC20s = await this.lookup(accountId)
-                const newAccountERC20s: AccountERC20[] = []
-                for (const accountERC20 of accountERC20s) {
-                    let newAccountERC20: AccountERC20
-                    if (accountERC20.erc20Info.contractId === contractId) {
-                        const newBalance = await this.getBalance(contractId, accountId)
-                        newAccountERC20 = { balance: newBalance.toString(), erc20Info: accountERC20.erc20Info}
-                    } else {
-                        newAccountERC20 = accountERC20
+                const accountAddress = await AccountByIdCache.instance.findAccountAddress(accountId)
+                const contractAddress = await ContractByIdCache.instance.findContractAddress(contractId)
+                if (accountAddress !== null && contractAddress !== null) {
+                    const accountERC20s = await this.lookup(accountId)
+                    const newAccountERC20s: AccountERC20[] = []
+                    for (const accountERC20 of accountERC20s) {
+                        let newAccountERC20: AccountERC20|null
+                        if (accountERC20.erc20Info.contractId === contractId) {
+                            const newBalance = await ERCUtils.loadBalance(contractAddress, accountAddress)
+                            if (newBalance !== null) {
+                                newAccountERC20 = { balance: newBalance.toString(), erc20Info: accountERC20.erc20Info}
+                            } else {
+                                newAccountERC20 = null
+                            }
+                        } else {
+                            newAccountERC20 = accountERC20
+                        }
+                        if (newAccountERC20 !== null) {
+                            newAccountERC20s.push(newAccountERC20)
+                        }
                     }
-                    newAccountERC20s.push(newAccountERC20)
+                    newPromise = Promise.resolve(newAccountERC20s)
+                } else {
+                    newPromise = Promise.resolve([])
                 }
-                newPromise = Promise.resolve(newAccountERC20s)
             } catch(reason) {
                 newPromise = Promise.reject(reason)
             }
@@ -66,12 +76,18 @@ export class AccountERC20Cache extends EntityCache<string, AccountERC20[]> {
     protected async load(accountId: string): Promise<AccountERC20[]> {
         let result: AccountERC20[] = []
 
-        const erc20Contracts = await ERC20Cache.instance.lookup()
-        for (const c of erc20Contracts) {
-            const erc20info = await ERC20InfoCache.instance.lookup(c.contractId)
-            const balance = await this.getBalance(c.contractId, accountId)
-            if (erc20info !== null && balance > 0) {
-                result.push( { balance: balance.toString(), erc20Info: erc20info })
+        const accountAddress = await AccountByIdCache.instance.findAccountAddress(accountId)
+        if (accountAddress !== null) {
+            const erc20Contracts = await ERC20Cache.instance.lookup()
+            for (const c of erc20Contracts) {
+                const contractAddress = await ContractByIdCache.instance.findContractAddress(c.contractId)
+                if (contractAddress !== null) {
+                    const erc20info = await ERC20InfoCache.instance.lookup(c.contractId)
+                    const balance = await ERCUtils.loadBalance(contractAddress, accountAddress)
+                    if (erc20info !== null && balance !== null && balance > 0) {
+                        result.push( { balance: balance.toString(), erc20Info: erc20info })
+                    }
+                }
             }
         }
         return Promise.resolve(result)
@@ -80,24 +96,6 @@ export class AccountERC20Cache extends EntityCache<string, AccountERC20[]> {
     //
     // Private
     //
-
-    private async getBalance(contractId: string, accountId: string): Promise<bigint> {
-        const contractAddress = await ContractByIdCache.instance.findContractAddress(contractId) ?? contractId
-        const accountAddress = await AccountByIdCache.instance.findAccountAddress(accountId) ?? accountId
-        const abi = [
-            "function balanceOf(address owner) view returns (uint256)",
-        ]
-        const itf = new ethers.Interface(abi)
-        const functionData = itf.encodeFunctionData(abi[0], [accountAddress])
-
-        const url = "api/v1/contracts/call"
-        const body: ContractCallRequest = {
-            data: functionData,
-            to: contractAddress,
-        }
-        const response = await axios.post<ContractCallResponse>(url, body)
-        return ethers.getBigInt(response.data.result)
-    }
 }
 
 export interface AccountERC20 {
