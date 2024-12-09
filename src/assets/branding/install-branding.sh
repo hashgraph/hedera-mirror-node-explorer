85803213810a30fcd46ff3304532660055baab2e
Simon Viénot <simon.vienot@icloud.com>
2024-12-09 13:46:23 +0100

    install-branding.sh also copies content of ${BRANDING_DIR}/public recursively.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- src/assets/branding/install-branding.sh
+++ src/assets/branding/install-branding.sh
@@ -7,7 +7,8 @@ cp ./src/assets/styles/brand-theme.scss ./src/assets/branding/
 # Install possible assets customization
 BRANDING_DIR="${BRANDING_LOCATION:-./branding}"
 cp ${BRANDING_DIR}/assets/* ./src/assets/branding/ 2>/dev/null | :
-cp ${BRANDING_DIR}/public/* ./public 2>/dev/null | :
+cp -R ${BRANDING_DIR}/public/* ./public 2>/dev/null | :
+cp -R ${BRANDING_DIR}/public/.* ./public 2>/dev/null | :
 cp ${BRANDING_DIR}/.env ./ 2>/dev/null | :
 cp ${BRANDING_DIR}/index.html ./ 2>/dev/null | :
 
