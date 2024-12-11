f678ca3b7a89ab45d720689479a46f0ff43cd9aa
Eric Le Ponner <eric.leponner@icloud.com>
2024-12-11 12:22:42 +0100

    Temporarily skip KNS test in SearchBar.cy.ts.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- tests/e2e/specs/SearchBar.cy.ts
+++ tests/e2e/specs/SearchBar.cy.ts
@@ -252,7 +252,7 @@ describe('Search Bar', () => {
         clickTestBody(searchAccount, '/mainnet/account/' + searchAccount, 'Account ID:', true)
     })
 
-    it('should find account with kns name', () => {
+    it.skip('should find account with kns name', () => {
         const searchName = "kabuto.hh"
         const searchAccount = "0.0.1001"
         testBody(searchAccount, '/mainnet/account/' + searchAccount, 'Account ID:', true, searchName)
