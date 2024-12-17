f0638c1c8f46d424a3791a10296945cc9273a8b7
Simon Viénot <simon.vienot@icloud.com>
2024-12-17 16:35:19 +0100

    Get rid of:  - brand-theme.scss  - explorer.scss  - explorer-bulma.sass
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- src/assets/branding/install-branding.sh
+++ src/assets/branding/install-branding.sh
@@ -2,7 +2,7 @@
 
 # Install default logos and theme
 cp ./src/assets/brand*.png ./src/assets/branding/
-cp ./src/assets/styles/brand-theme.scss ./src/assets/branding/
+cp ./src/assets/styles/brand-theme.css ./src/assets/branding/
 
 # Install possible assets customization
 BRANDING_DIR="${BRANDING_LOCATION:-./branding}"
@@ -13,7 +13,7 @@ cp ${BRANDING_DIR}/.env ./ 2>/dev/null | :
 cp ${BRANDING_DIR}/index.html ./ 2>/dev/null | :
 
 # When custom theme provided: pre-process SASS/SCSS files based on it
-npx sass ./src/assets/branding/brand-theme.scss ./src/assets/branding/brand-theme.css
-npx sass ./src/assets/styles/explorer.scss ./src/assets/styles/explorer.css
-npx sass ./src/assets/styles/explorer-bulma.sass ./src/assets/styles/explorer-bulma.css
+#npx sass ./src/assets/branding/brand-theme.scss ./src/assets/branding/brand-theme.css
+#npx sass ./src/assets/styles/explorer.scss ./src/assets/styles/explorer.css
+#npx sass ./src/assets/styles/explorer-bulma.sass ./src/assets/styles/explorer-bulma.css
 npx sass ./src/assets/styles/explorer-oruga.scss ./src/assets/styles/explorer-oruga.css
