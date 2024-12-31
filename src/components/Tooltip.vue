d674467d8df10107beda09a8b8165004f117fb5f
Simon Viénot <simon.vienot@icloud.com>
2024-12-31 17:13:44 +0100

    Add Tooltip component (for further use).
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

---
+++ src/components/Tooltip.vue
@@ -0,0 +1,69 @@
+<!--
+  -
+  - Hedera Mirror Node Explorer
+  -
+  - Copyright (C) 2021 - 2024 Hedera Hashgraph, LLC
+  -
+  - Licensed under the Apache License, Version 2.0 (the "License");
+  - you may not use this file except in compliance with the License.
+  - You may obtain a copy of the License at
+  -
+  -      http://www.apache.org/licenses/LICENSE-2.0
+  -
+  - Unless required by applicable law or agreed to in writing, software
+  - distributed under the License is distributed on an "AS IS" BASIS,
+  - WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  - See the License for the specific language governing permissions and
+  - limitations under the License.
+  -
+  -->
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                     TEMPLATE                                                    -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<template>
+
+  <o-tooltip v-if="props.text" :label="props.text" :delay="delay" multiline class="tooltip">
+    <slot/>
+  </o-tooltip>
+
+  <slot v-else/>
+
+</template>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                      SCRIPT                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<script setup lang="ts">
+
+import {computed, PropType} from 'vue';
+
+const props = defineProps({
+  text: {
+    type: String as PropType<string | null>,
+    default: null
+  },
+  delay: {
+    type: Number as PropType<number | null>,
+    default: null
+  }
+})
+
+const DEFAULT_DELAY = 500
+const delay = computed(() => props.delay ?? DEFAULT_DELAY)
+
+</script>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                       STYLE                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<style scoped>
+
+.tooltip {
+  cursor: help;
+}
+
+</style>
