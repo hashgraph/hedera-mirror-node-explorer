2abeba6207f465ff3b22e922420f112a80ad6220
Simon Viénot <simon.vienot@icloud.com>
2025-01-06 13:06:19 +0100

    Replace ContractCallTransactionTable and CryptoTransactionTable by SimpleTransactionTable.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- src/components/dashboard/SimpleTransactionTable.vue
+++ src/components/dashboard/SimpleTransactionTable.vue
@@ -27,15 +27,7 @@
   <o-table
       :data="transactions"
       :loading="loading"
-      paginated
-      backend-pagination
-      pagination-order="left"
-      :range-before="0"
-      :range-after="0"
-      :total="total"
-      v-model:current-page="currentPage"
       :per-page="perPage"
-      @page-change="onPageChange"
       @cell-click="handleClick"
 
       :hoverable="true"
@@ -43,32 +35,20 @@
       :striped="true"
       :mobile-breakpoint="ORUGA_MOBILE_BREAKPOINT"
 
-      aria-current-label="Current page"
-      aria-next-label="Next page"
-      aria-page-label="Page"
-      aria-previous-label="Previous page"
       customRowKey="consensus_timestamp"
-      default-sort="consensus_timestamp"
   >
 
     <o-table-column v-slot="props" field="transaction_id" label="ID">
-      <TransactionLabel v-bind:transaction-id="props.row.transaction_id" v-bind:result="props.row.result"/>
+      <TransactionLabel :transaction-id="props.row.transaction_id" :result="props.row.result"/>
     </o-table-column>
 
-    <o-table-column v-slot="props" label="Content">
-      <TransactionSummary v-bind:transaction="props.row"/>
+    <o-table-column v-slot="props" label="CONTENT">
+      <TransactionSummary :transaction="props.row"/>
     </o-table-column>
 
-    <o-table-column v-slot="props" field="consensus_timestamp" label="Time">
-      <TimestampValue v-bind:timestamp="props.row.consensus_timestamp"/>
+    <o-table-column v-slot="props" field="consensus_timestamp" label="TIME">
+      <TimestampValue :timestamp="props.row.consensus_timestamp"/>
     </o-table-column>
-
-    <template v-slot:bottom-left>
-      <TablePageSize
-          v-model:size="perPage"
-          :storage-key="AppStorage.BOTTOM_DASHBOARD_TABLE_PAGE_SIZE_KEY"
-      />
-    </template>
 
   </o-table>
 
@@ -80,53 +60,32 @@
 <!--                                                      SCRIPT                                                     -->
 <!-- --------------------------------------------------------------------------------------------------------------- -->
 
-<script lang="ts">
+<script setup lang="ts">
 
-import {ComputedRef, defineComponent, PropType, Ref} from 'vue';
+import {PropType} from 'vue';
 import {Transaction} from "@/schemas/MirrorNodeSchemas";
-import {routeManager} from "@/router";
 import TimestampValue from "@/components/values/TimestampValue.vue";
 import TransactionLabel from "@/components/values/TransactionLabel.vue";
+import {routeManager} from "@/router";
 import TransactionSummary from "@/components/transaction/TransactionSummary.vue";
 import {ORUGA_MOBILE_BREAKPOINT} from "@/BreakPoints";
 import EmptyTable from "@/components/EmptyTable.vue";
 import {TransactionTableController} from "@/components/transaction/TransactionTableController";
-import TablePageSize from "@/components/transaction/TablePageSize.vue";
-import {AppStorage} from "@/AppStorage";
 
-export default defineComponent({
-  name: 'ContractCallTransactionTable',
-
-  components: {TablePageSize, EmptyTable, TransactionSummary, TimestampValue, TransactionLabel},
-
-  props: {
-    controller: {
-      type: Object as PropType<TransactionTableController>,
-      required: true
-    }
-  },
-
-  setup(props) {
-
-    const handleClick = (t: Transaction, c: unknown, i: number, ci: number, event: MouseEvent) => {
-      routeManager.routeToTransaction(t, event)
-    }
-
-    return {
-      transactions: props.controller.rows as ComputedRef<Transaction[]>,
-      loading: props.controller.loading as ComputedRef<boolean>,
-      total: props.controller.totalRowCount as ComputedRef<number>,
-      currentPage: props.controller.currentPage as Ref<number>,
-      onPageChange: props.controller.onPageChange,
-      perPage: props.controller.pageSize as Ref<number>,
-      handleClick,
-      AppStorage,
-
-      // From App
-      ORUGA_MOBILE_BREAKPOINT,
-    }
+const props = defineProps({
+  controller: {
+    type: Object as PropType<TransactionTableController>,
+    required: true
   }
-});
+})
+
+const handleClick = (t: Transaction, c: unknown, i: number, ci: number, event: MouseEvent) => {
+  routeManager.routeToTransaction(t, event)
+}
+
+const transactions = props.controller.rows
+const loading = props.controller.loading
+const perPage = props.controller.pageSize
 
 </script>
 
