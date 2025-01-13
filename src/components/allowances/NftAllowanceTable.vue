ede95fba2bfe59d7f1c07c28ac3ef74b57b7c789
Simon Viénot <simon.vienot@icloud.com>
2025-01-13 11:18:32 +0100

    Revamp and convert NftAllowanceTable to script setup.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- src/components/allowances/NftAllowanceTable.vue
+++ src/components/allowances/NftAllowanceTable.vue
@@ -32,9 +32,9 @@
       :mobile-breakpoint="ORUGA_MOBILE_BREAKPOINT"
       :narrowed="true"
       :paginated="paginated"
-      pagination-order="left"
-      :range-before="0"
-      :range-after="0"
+      pagination-order="centered"
+      :range-before="1"
+      :range-after="1"
       :per-page="perPage"
       :striped="true"
 
@@ -49,28 +49,26 @@
       default-sort="spender"
       @page-change="onPageChange">
 
-    <o-table-column v-slot="props" field="token" label="Token ID">
-      <TokenLink :token-id="props.row.token_id" :show-extra="true"/>
+    <o-table-column v-slot="props" field="token" label="TOKEN ID">
+      <TokenLink class="entity-id" :token-id="props.row.token_id" :show-extra="true"/>
     </o-table-column>
 
-    <o-table-column v-slot="props" field="serial" label="Serial #">
+    <o-table-column v-slot="props" field="serial" label="SERIAL #">
       <div class="is-numeric">
         {{ props.row.serial_number }}
       </div>
     </o-table-column>
 
-    <o-table-column v-slot="props" field="spender" label="Spender">
+    <o-table-column v-slot="props" field="spender" label="SPENDER">
       <AccountLink :account-id="props.row.spender" :show-extra="true"/>
     </o-table-column>
 
-    <o-table-column v-slot="props" field="timestamp" label="Time">
+    <o-table-column v-slot="props" field="timestamp" label="TIME">
       <TimestampValue v-bind:timestamp="props.row.modified_timestamp"/>
     </o-table-column>
 
     <o-table-column v-if="isWalletConnected" v-slot="props" field="action" position="right">
-      <span class="h-is-property-text icon is-small">
-        <i class="far fa-trash-alt" @click="$emit('deleteAllowance', props.row)"></i>
-      </span>
+      <i class="far fa-trash-alt" @click="emit('deleteAllowance', props.row)"/>
     </o-table-column>
 
     <template v-slot:bottom-left>
@@ -97,10 +95,9 @@
 <!--                                                      SCRIPT                                                     -->
 <!-- --------------------------------------------------------------------------------------------------------------- -->
 
-<script lang="ts">
+<script setup lang="ts">
 
-import {computed, ComputedRef, defineComponent, inject, PropType, Ref} from 'vue';
-import {Nft} from "@/schemas/MirrorNodeSchemas";
+import {computed, PropType} from 'vue';
 import {ORUGA_MOBILE_BREAKPOINT} from "@/BreakPoints";
 import TimestampValue from "@/components/values/TimestampValue.vue";
 import EmptyTable from "@/components/EmptyTable.vue";
@@ -111,49 +108,28 @@ import {NftAllowanceTableController} from "@/components/allowances/NftAllowanceT
 import TablePageSize from "@/components/transaction/TablePageSize.vue";
 import {AppStorage} from "@/AppStorage";
 
-export default defineComponent({
-  name: 'NftAllowanceTable',
+const emit = defineEmits(["deleteAllowance"])
 
-  components: {TablePageSize, TokenLink, AccountLink, EmptyTable, TimestampValue},
-
-  emits: ["deleteAllowance"],
-
-  props: {
-    controller: {
-      type: Object as PropType<NftAllowanceTableController>,
-      required: true
-    }
-  },
-
-  setup: function (props) {
-    const isTouchDevice = inject('isTouchDevice', false)
-    const isSmallScreen = inject('isSmallScreen', true)
-    const isMediumScreen = inject('isMediumScreen', true)
-
-    const isWalletConnected = computed(
-        () => walletManager.isHieroWallet.value
-            && walletManager.accountId.value === props.controller.accountId.value
-    )
-
-    return {
-      isTouchDevice,
-      isSmallScreen,
-      isMediumScreen,
-      isWalletConnected,
-      nfts: props.controller.rows as ComputedRef<Nft[]>,
-      loading: props.controller.loading as ComputedRef<boolean>,
-      total: props.controller.totalRowCount as ComputedRef<number>,
-      currentPage: props.controller.currentPage as Ref<number>,
-      onPageChange: props.controller.onPageChange,
-      perPage: props.controller.pageSize as Ref<number>,
-      paginated: props.controller.paginated as ComputedRef<boolean>,
-      showPageSizeSelector: props.controller.showPageSizeSelector as ComputedRef<boolean>,
-      AppStorage,
-      // From App
-      ORUGA_MOBILE_BREAKPOINT,
-    }
+const props = defineProps({
+  controller: {
+    type: Object as PropType<NftAllowanceTableController>,
+    required: true
   }
-});
+})
+
+const isWalletConnected = computed(() =>
+    walletManager.isHieroWallet.value
+    && walletManager.accountId.value === props.controller.accountId.value
+)
+
+const nfts = props.controller.rows
+const loading = props.controller.loading
+const total = props.controller.totalRowCount
+const currentPage = props.controller.currentPage
+const onPageChange = props.controller.onPageChange
+const perPage = props.controller.pageSize
+const paginated = props.controller.paginated
+const showPageSizeSelector = props.controller.showPageSizeSelector
 
 </script>
 
@@ -162,5 +138,9 @@ export default defineComponent({
 <!-- --------------------------------------------------------------------------------------------------------------- -->
 
 <style scoped>
+
+.entity-id {
+  font-weight: 600;
+}
 
 </style>
