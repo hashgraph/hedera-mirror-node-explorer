8db1d07ff3c479acce8d821529a80a13034ae1a8
Nathan Klick <nathan@swirldslabs.com>
2025-01-20 11:24:24 -0600

    fix: resolve footer version issue
    Signed-off-by: Nathan Klick <nathan@swirldslabs.com>

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,21 @@ import vue from '@vitejs/plugin-vue'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import {nodePolyfills} from 'vite-plugin-node-polyfills'
 import * as child from "child_process"
+import {readFileSync} from "fs"
 
 const commitHash = child.execSync("git rev-parse --short HEAD").toString() //i.e., 706e821
-const tagAndCommitHash = child.execSync("git describe --always --tags").toString() //i.e., v23.5.0-1-g706e821
+const tagAndCommitHash = resolveVersion() //i.e., v23.5.0-1-g706e821
 const buildDate = new Date().toUTCString()
+
+function resolveVersion() {
+  try {
+    const packageJson = JSON.parse(readFileSync('./package.json', 'utf8'))
+    return `v${packageJson.version}` || child.execSync("git describe --always --tags").toString()
+  } catch (e) {
+    console.error('Could not read package.json')
+    return child.execSync("git describe --always --tags").toString()
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
