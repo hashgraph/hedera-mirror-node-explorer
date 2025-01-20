0278afd391f468e20812dd8a47cb5f545cf55d3a
Eric Le Ponner <eric.leponner@icloud.com>
2025-01-20 18:46:48 +0100

    Added RadioBoxView and StackView (to be used later).
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

---
+++ src/components/RabioBoxView.vue
@@ -0,0 +1,64 @@
+<!--
+  -
+  - Hedera Mirror Node Explorer
+  -
+  - Copyright (C) 2021 - 2024 Hedera Hashgraph, LLC
+  -
+  - Licensed under the Apache License, Version 2.0 (the "License");
+  - you may not use this file except in compliance with the License.
+  - You may obtain a copy of the License at
+  -
+  -      http://www.apache.org/licenses/LICENSE-2.0
+  -
+  - Unless required by applicable law or agreed to in writing, software
+  - distributed under the License is distributed on an "AS IS" BASIS,
+  - WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  - See the License for the specific language governing permissions and
+  - limitations under the License.
+  -
+  -->
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                     TEMPLATE                                                    -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<template>
+  <label style="font-size: 14px">
+    <input type="radio" name="stakeTarget" :value="value" v-model="model" style="vertical-align: middle"/>
+    <span style="padding-left: 6px">
+      <slot/>
+    </span>
+  </label>
+</template>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                      SCRIPT                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<script setup lang="ts">
+
+const model = defineModel({
+  type: String,
+  required: true
+})
+
+const props = defineProps({
+  value: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  }
+})
+
+</script>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                       STYLE                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<style scoped>
+
+</style>
