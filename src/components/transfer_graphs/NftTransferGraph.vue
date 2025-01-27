f8a8cf164fbd4a0e253b1aba8d723a32384b31b7
Simon Viénot <simon.vienot@icloud.com>
2025-01-27 11:21:11 +0100

    Revamp NftTransferGraph.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- src/components/transfer_graphs/NftTransferGraph.vue
+++ src/components/transfer_graphs/NftTransferGraph.vue
@@ -25,69 +25,69 @@
 <template>
 
   <div v-if="nftTransferLayout.length >= 1">
-    <div v-if="!compact" class="h-is-tertiary-text mb-2">NFT Transfers</div>
+    <div v-if="!compact" class="h-sub-section">
+      NFT Transfers
+    </div>
 
-    <div class="graph-container" v-bind:class="{'graph-container-6': !compact && descriptionVisible}">
+    <div class="graph-container" :class="{'graph-container-6': !compact && descriptionVisible}">
 
       <template v-if="!compact">
 
-        <div class="h-is-text-size-3 has-text-grey-light has-text-weight-light mb-2">Account</div>
+        <div class="transfer-header">ACCOUNT</div>
         <div/>
-        <div class="h-is-text-size-3 has-text-grey-light has-text-weight-light mb-2">Non Fungible Tokens</div>
+        <div class="transfer-header">NFT</div>
         <div/>
-        <div class="h-is-text-size-3 has-text-grey-light has-text-weight-light mb-2">Account</div>
+        <div class="transfer-header">ACCOUNT</div>
         <div v-if="!compact && descriptionVisible"/>
       </template>
 
-      <template v-for="i in nftTransferLayout.length" v-bind:key="i">
+      <template v-for="i in nftTransferLayout.length" :key="i">
 
         <!-- #0 : account id -->
-        <div>
+        <div class="transfer-account">
           <AccountLink
-              v-bind:account-id="nftTransferLayout[i-1].sender_account_id"
-              v-bind:no-anchor="compact"
+              :account-id="nftTransferLayout[i-1].sender_account_id"
+              :no-anchor="compact"
               null-label="MINT"
               data-cy="sourceAccount"/>
         </div>
 
         <!-- #1 : arrow -->
         <div style="position: relative">
-          <ArrowSegment v-bind:compact="compact"/>
+          <ArrowSegment :compact="compact"/>
         </div>
 
         <!-- #2 : nfts -->
-        <div>
+        <div class="transfer-token">
           <TokenLink
-              v-bind:token-id="nftTransferLayout[i-1].token_id ?? undefined"
-              v-bind:show-extra="true"
-              v-bind:no-anchor="compact"
+              :token-id="nftTransferLayout[i-1].token_id ?? undefined"
+              :show-extra="true"
+              :no-anchor="compact"
               data-cy="nft"/>
-          <div class="h-is-text-size-3" style="max-width: 200px">
-            <template v-if="!compact">
+          <div v-if="!compact" class="transfer-serial">
               <span v-for="sn in nftTransferLayout[i-1].serial_numbers" :key="sn">
                 #{{ sn }}
               </span>
-            </template>
           </div>
         </div>
 
         <!-- #3 : arrow -->
         <div style="position: relative">
-          <ArrowSegment v-bind:compact="compact"/>
+          <ArrowSegment :compact="compact"/>
         </div>
 
         <!-- #4 : account id -->
-        <div>
+        <div class="transfer-account">
           <AccountLink
-              v-bind:account-id="nftTransferLayout[i-1].receiver_account_id"
-              v-bind:no-anchor="compact"
+              :account-id="nftTransferLayout[i-1].receiver_account_id"
+              :no-anchor="compact"
               null-label="BURN"
               data-cy="destinationAccount"/>
         </div>
 
         <!-- #5 : description -->
-        <div v-if="!compact && descriptionVisible">
-          <span class="h-is-smaller">{{ nftTransferLayout[i-1].description }}</span>
+        <div v-if="!compact && descriptionVisible" class="description">
+          {{ nftTransferLayout[i - 1].description }}
         </div>
 
       </template>
@@ -101,41 +101,30 @@
 <!--                                                      SCRIPT                                                     -->
 <!-- --------------------------------------------------------------------------------------------------------------- -->
 
-<script lang="ts">
+<script setup lang="ts">
 
-import {defineComponent, inject, PropType, ref, watch} from "vue";
+import {inject, PropType, ref, watch} from "vue";
 import AccountLink from "@/components/values/link/AccountLink.vue";
 import TokenLink from "@/components/values/link/TokenLink.vue";
 import ArrowSegment from "@/components/transfer_graphs/ArrowSegment.vue";
 import {NFTTransferLayout} from "@/components/transfer_graphs/layout/NFTTransferLayout";
 import {TransactionDetail} from "@/schemas/MirrorNodeSchemas";
 
-export default defineComponent({
-  name: "NftTransferGraph",
-  components: {TokenLink, AccountLink, ArrowSegment},
-  props: {
-    transaction: Object as PropType<TransactionDetail>,
-    compact: {
-      type: Boolean,
-      default: false
-    }
-  },
-  setup(props) {
-
-    const nftTransferLayout = ref(NFTTransferLayout.make(props.transaction))
-
-    watch(() => props.transaction, () => {
-      nftTransferLayout.value = NFTTransferLayout.make(props.transaction)
-    })
-
-    const descriptionVisible = inject("isSmallScreen", true)
-
-    return {
-      nftTransferLayout,
-      descriptionVisible
-    }
+const props = defineProps({
+  transaction: Object as PropType<TransactionDetail>,
+  compact: {
+    type: Boolean,
+    default: false
   }
 })
+
+const nftTransferLayout = ref(NFTTransferLayout.make(props.transaction))
+
+watch(() => props.transaction, () => {
+  nftTransferLayout.value = NFTTransferLayout.make(props.transaction)
+})
+
+const descriptionVisible = inject("isSmallScreen", true)
 
 </script>
 
@@ -146,15 +135,44 @@ export default defineComponent({
 <style scoped>
 
 .graph-container {
+  column-gap: 1em;
   display: inline-grid;
+  font-family: Inter, sans-serif;
+  font-size: 14px;
   grid-template-columns: repeat(5, auto);
   line-height: 1.4rem;
-  column-gap: 1em;
+  padding-left: 16px;
+  padding-top: 8px;
 }
 
 .graph-container-6 {
   grid-template-columns: repeat(6, auto);
-  line-height: 1.4rem;
+}
+
+div.transfer-header {
+  color: var(--text-secondary);
+  font-weight: 500;
+  font-size: 12px;
+  font-family: Inter, sans-serif;
+}
+
+div.transfer-account {
+  color: var(--text-primary);
+  font-weight: 700;
+}
+
+div.transfer-token {
+  color: var(--text-primary);
+  font-weight: 400;
+}
+
+div.transfer-serial {
+  color: var(--text-secondary);
+  max-width: 200px;
+}
+
+div.description {
+  color: var(--text-secondary);
 }
 
 </style>
