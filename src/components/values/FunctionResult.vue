8443d5179eae360408dc30e0e7fcdec909d972c0
Simon Viénot <simon.vienot@icloud.com>
2025-01-27 11:10:04 +0100

    Revamp FunctionResult.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- src/components/values/FunctionResult.vue
+++ src/components/values/FunctionResult.vue
@@ -30,7 +30,9 @@
 
     <template v-for="result in outputs" :key="result.name">
       <Property :custom-nb-col-class="customNbColClass">
-        <template v-slot:name>{{ result.name }}</template>
+        <template v-slot:name>
+          <span style="padding-left: 16px;">{{ result.name }}</span>
+        </template>
         <template v-slot:value>
           <FunctionValue :ntv="result"/>
         </template>
@@ -44,7 +46,7 @@
       <template v-slot:name>Output Result</template>
       <template v-slot:value>
         <ByteCodeValue :byte-code="output ?? undefined" :height-in-pixel="140"/>
-        <div v-if="outputDecodingStatus" class="h-is-extra-text h-is-text-size-3">
+        <div v-if="outputDecodingStatus" class="h-is-extra-text">
           <span class="icon fas fa-exclamation-circle has-text-grey is-small mt-1 mr-1"/>
           <span>{{ outputDecodingStatus }}</span>
         </div>
@@ -59,42 +61,29 @@
 <!--                                                      SCRIPT                                                     -->
 <!-- --------------------------------------------------------------------------------------------------------------- -->
 
-<script lang="ts">
+<script setup lang="ts">
 
-import {defineComponent, inject, PropType, ref} from 'vue';
-import {initialLoadingKey} from "@/AppKeys";
+import {PropType} from 'vue';
 import {FunctionCallAnalyzer} from "@/utils/analyzer/FunctionCallAnalyzer";
 import Property from "@/components/Property.vue";
 import FunctionValue from "@/components/values/FunctionValue.vue";
 import ByteCodeValue from "@/components/values/ByteCodeValue.vue";
 
-export default defineComponent({
-  name: 'FunctionResult',
-  components: {ByteCodeValue, FunctionValue, Property},
-  props: {
-    analyzer: {
-      type: Object as PropType<FunctionCallAnalyzer>,
-      required: true
-    },
-    customNbColClass: String,
-    showNone: {
-      type: Boolean,
-      default: false
-    }
+const props = defineProps({
+  analyzer: {
+    type: Object as PropType<FunctionCallAnalyzer>,
+    required: true
   },
-
-  setup(props) {
-
-    const initialLoading = inject(initialLoadingKey, ref(false))
-    return {
-      output: props.analyzer.normalizedOutput,
-      signature: props.analyzer.signature,
-      outputs: props.analyzer.outputs,
-      outputDecodingStatus: props.analyzer.outputDecodingStatus,
-      initialLoading
-    }
+  customNbColClass: String,
+  showNone: {
+    type: Boolean,
+    default: false
   }
-});
+})
+
+const output = props.analyzer.normalizedOutput
+const outputs = props.analyzer.outputs
+const outputDecodingStatus = props.analyzer.outputDecodingStatus
 
 </script>
 
