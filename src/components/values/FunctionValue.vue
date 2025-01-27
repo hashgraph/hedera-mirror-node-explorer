f75fe73992d3432fed2d9d9b5ecb8ac615614d1e
Simon Viénot <simon.vienot@icloud.com>
2025-01-27 11:13:08 +0100

    Revamp FunctionValue.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- src/components/values/FunctionValue.vue
+++ src/components/values/FunctionValue.vue
@@ -27,16 +27,15 @@
   <div v-if="value">
     <EVMAddress v-if="addressValue" :address="addressValue" :compact="!isSmallScreen && !isMediumScreen"/>
     <div v-else :class="{'has-text-grey': lowContrast}"
-         class="is-family-monospace h-is-text-size-3 should-wrap is-flex">
+         class="should-wrap is-flex">
       <p class="mr-1">{{ value }}</p>
 
       <p v-if="ntv?.comment"
-         target="_blank"
-         class="has-text-grey h-is-extra-text is-family-monospace">
+         class="h-is-extra-text">
         ({{ ntv.comment }})
       </p>
     </div>
-    <div v-if="!hideType" class="h-is-extra-text h-is-text-size-3">{{ type }}</div>
+    <div v-if="!hideType" class="h-is-extra-text">{{ type }}</div>
   </div>
   <div v-else-if="initialLoading"/>
   <div v-else class="has-text-grey">None</div>
@@ -47,49 +46,37 @@
 <!--                                                      SCRIPT                                                     -->
 <!-- --------------------------------------------------------------------------------------------------------------- -->
 
-<script lang="ts">
+<script setup lang="ts">
 
-import {computed, defineComponent, inject, PropType, ref} from 'vue';
+import {computed, inject, PropType, ref} from 'vue';
 import {initialLoadingKey} from "@/AppKeys";
 import EVMAddress from "@/components/values/EVMAddress.vue";
 import {NameTypeValue} from "@/utils/analyzer/FunctionCallAnalyzer";
 
-export default defineComponent({
-  name: 'FunctionValue',
-  components: {EVMAddress},
-  props: {
-    ntv: Object as PropType<NameTypeValue>,
-    hideType: {
-      type: Boolean,
-      default: false,
-      required: false,
-    },
-    lowContrast: {
-      type: Boolean,
-      default: false,
-      required: false
-    }
+const props = defineProps({
+  ntv: Object as PropType<NameTypeValue>,
+  hideType: {
+    type: Boolean,
+    default: false,
+    required: false,
   },
-
-  setup(props) {
-    const isSmallScreen = inject('isSmallScreen', true)
-    const isMediumScreen = inject('isMediumScreen', true)
-    const initialLoading = inject(initialLoadingKey, ref(false))
-
-    const addressValue = computed(() => {
-      return props.ntv?.type === 'address' ? props.ntv.value as string : null
-    })
-
-    return {
-      isSmallScreen,
-      isMediumScreen,
-      initialLoading,
-      type: props.ntv?.type,
-      value: props.ntv?.value?.toString(),
-      addressValue,
-    }
+  lowContrast: {
+    type: Boolean,
+    default: false,
+    required: false
   }
-});
+})
+
+const isSmallScreen = inject('isSmallScreen', true)
+const isMediumScreen = inject('isMediumScreen', true)
+const initialLoading = inject(initialLoadingKey, ref(false))
+
+const addressValue = computed(() => {
+  return props.ntv?.type === 'address' ? props.ntv.value as string : null
+})
+
+const type = props.ntv?.type
+const value = props.ntv?.value?.toString()
 
 </script>
 
