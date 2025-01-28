01b39fc73bb1966ef97d57049c62a027ee2a9867
Eric Le Ponner <eric.leponner@icloud.com>
2025-01-28 19:26:37 +0100

    RejectTokenDialog is now based on TransactionGroupDialog.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- src/components/token/TokensSection.vue
+++ src/components/token/TokensSection.vue
@@ -169,7 +169,7 @@ import {FungibleTableController} from "@/components/account/FungibleTableControl
 import {DialogController} from "@/dialogs/core/dialog/DialogController.ts";
 import {routeManager, walletManager} from "@/router";
 import {Nft, Token, TokenAirdrop, TokenType} from "@/schemas/MirrorNodeSchemas";
-import RejectTokenDialog from "@/dialogs/transaction/RejectTokenDialog.vue";
+import RejectTokenDialog from "@/dialogs/transaction/token/RejectTokenDialog.vue";
 import {PendingAirdropTableController} from "@/components/account/PendingAirdropTableController";
 import PendingNftAirdropTable from "@/components/account/PendingNftAirdropTable.vue";
 import ClaimTokenDialog from "@/components/account/ClaimTokenDialog.vue";
