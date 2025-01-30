f368b743a743e2cced54a88e02b6467c64904f61
Simon Viénot <simon.vienot@icloud.com>
2025-01-30 16:18:14 +0100

    Revamp Copyable.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- src/components/Copyable.vue
+++ src/components/Copyable.vue
@@ -23,16 +23,12 @@
 <!-- --------------------------------------------------------------------------------------------------------------- -->
 
 <template>
-  <div class="shy-scope" style="display: inline-block; position: relative;">
+  <div class="shy-scope">
     <slot name="content"/>
-    <div v-if="enableCopy && contentToCopy" id="shyCopyButton" class="shy"
-         style="position: absolute; left: 0; top: 0; width: 100%; height: 100%">
-      <div
-          style="position: absolute; left: 0; top: 0; width: 100%; height: 100%; background: rgba(0, 0, 0, 0.50)"></div>
-      <div v-if="enableCopy"
-           style="position: absolute; display: inline-block; left: 50%; top: 50%; transform: translate(-50%, -50%);">
-        <button class="button is-dark h-is-text-size-3"
-                v-on:click.stop="copyToClipboard">Copy</button>
+    <div v-if="enableCopy && contentToCopy" id="shyCopyButton" class="shy">
+      <div class="copy-mask"/>
+      <div v-if="enableCopy" class="copy-button-container">
+        <button class="copy-button" v-on:click.stop="copyToClipboard">Copy</button>
       </div>
     </div>
   </div>
@@ -42,32 +38,24 @@
 <!--                                                      SCRIPT                                                     -->
 <!-- --------------------------------------------------------------------------------------------------------------- -->
 
-<script lang="ts">
+<script setup lang="ts">
 
-import {defineComponent} from "vue";
-
-export default defineComponent({
-  name: "Copyable",
-  props: {
-    contentToCopy: String,
-    enableCopy: {
-      type: Boolean,
-      default: true
-    },
+const props = defineProps({
+  contentToCopy: String,
+  enableCopy: {
+    type: Boolean,
+    default: true
   },
-  emits: ['copyMade'],
-  setup(props, context) {
-    const copyToClipboard = (): void => {
-      if (props.contentToCopy?.length) {
-        navigator.clipboard.writeText(props.contentToCopy)
-        context.emit('copyMade')
-      }
-    }
-    return {
-      copyToClipboard
-    }
-  }
 })
+
+const emit = defineEmits(['copyMade'])
+
+const copyToClipboard = (): void => {
+  if (props.contentToCopy?.length) {
+    navigator.clipboard.writeText(props.contentToCopy)
+    emit('copyMade')
+  }
+}
 
 </script>
 
@@ -76,11 +64,56 @@ export default defineComponent({
 <!-- --------------------------------------------------------------------------------------------------------------- -->
 
 <style scoped>
-.shy {
-  display: none
+
+div.shy-scope {
+  display: inline-block;
+  position: relative;
 }
 
-.shy-scope:hover > .shy {
+div.shy {
+  display: none;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%
+}
+
+div.shy-scope:hover > div.shy {
   display: block;
 }
+
+div.copy-mask {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.50);
+  border-width: 0;
+  border-radius: 4px;
+}
+
+div.copy-button-container {
+  position: absolute;
+  display: inline-block;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+
+button.copy-button {
+  background-color: var(--copy-button-background);
+  border-radius: 16px;
+  color: var(--copy-button-text);
+  font-size: 12px;
+  height: 28px;
+  padding: 6px 12px;
+  width: 100px;
+}
+
+button.copy-button:active {
+  opacity: 75%;
+}
+
 </style>
