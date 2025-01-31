4f7274611d460999fe6dce623148c8bc703fdc4a
Simon Viénot <simon.vienot@icloud.com>
2025-01-31 14:08:23 +0100

    Use compact EVMAddress in ContractActionsTable and temporarily hide EXPANSE details capability on smaller screens (probable bug in Oruga table).
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- src/components/contract/ContractActionDetails.vue
+++ src/components/contract/ContractActionDetails.vue
@@ -23,101 +23,53 @@
 <!-- --------------------------------------------------------------------------------------------------------------- -->
 
 <template>
-  <template v-if="isMediumScreen">
-    <div class="columns pt-2 pb-0 mb-0">
-      <div class="column property-container">
-        <Property id="actionDetailFrom" :custom-nb-col-class="propertySizeClass">
-          <template v-slot:name>From</template>
-          <template v-slot:value>
-            <EVMAddress :id="action?.caller" :address="action?.from" :entity-type="action?.caller_type"
-                        :show-type="true"/>
-          </template>
-        </Property>
-        <Property id="actionDetailTo" :custom-nb-col-class="propertySizeClass">
-          <template v-slot:name>To</template>
-          <template v-slot:value>
-            <EVMAddress :id="action?.recipient" :address="action?.to" :entity-type="action?.recipient_type"
-                        :show-type="true"/>
-          </template>
-        </Property>
-      </div>
-      <div class="column property-container h-has-column-dashed-separator">
-        <Property id="actionDetailValue" :custom-nb-col-class="propertySizeClass">
-          <template v-slot:name>Value</template>
-          <template v-slot:value>
-            <HbarAmount :amount="action?.value"/>
-          </template>
-        </Property>
-        <Property id="actionDetailGasLimit" :custom-nb-col-class="propertySizeClass">
-          <template v-slot:name>Gas Limit</template>
-          <template v-slot:value>
-            <PlainAmount :amount="action?.gas"/>
-          </template>
-        </Property>
-        <Property id="actionDetailGasUsed" :custom-nb-col-class="propertySizeClass">
-          <template v-slot:name>Gas Used</template>
-          <template v-slot:value>
-            <PlainAmount :amount="action?.gas_used"/>
-          </template>
-        </Property>
-        <FunctionError :analyzer="functionCallAnalyzer" :custom-nb-col-class="propertySizeClass" :show-none="true"/>
-      </div>
-    </div>
-
-    <hr class="dotted"/>
-
-    <div class="columns pt-0 mt-0 pb-2">
-      <div class="column property-container">
-        <FunctionInput :analyzer="functionCallAnalyzer" :custom-nb-col-class="propertySizeClass" :show-none="true"/>
-      </div>
-      <div class="column property-container h-has-column-dashed-separator">
-        <FunctionResult :analyzer="functionCallAnalyzer" :custom-nb-col-class="propertySizeClass" :show-none="true"/>
-      </div>
-    </div>
-  </template>
-
-  <template v-else>
-    <div class="pt-2 pb-0 mb-0">
-      <Property id="actionDetailFrom" :custom-nb-col-class="propertySizeClass">
-        <template v-slot:name>From</template>
-        <template v-slot:value>
-          <EVMAddress :id="action?.caller" :address="action?.from" :entity-type="action?.caller_type"
-                      :show-type="true"/>
-        </template>
-      </Property>
-      <Property id="actionDetailTo" :custom-nb-col-class="propertySizeClass">
-        <template v-slot:name>To</template>
-        <template v-slot:value>
-          <EVMAddress :id="action?.recipient" :address="action?.to" :entity-type="action?.recipient_type"
-                      :show-type="true"/>
-        </template>
-      </Property>
-      <Property id="actionDetailGasUsed" :custom-nb-col-class="propertySizeClass">
-        <template v-slot:name>Gas Used</template>
-        <template v-slot:value>
-          <PlainAmount :amount="action?.gas_used"/>
-        </template>
-      </Property>
-      <FunctionError :analyzer="functionCallAnalyzer" :custom-nb-col-class="propertySizeClass" :show-none="true"/>
-      <FunctionInput :analyzer="functionCallAnalyzer" :custom-nb-col-class="propertySizeClass" :show-none="true"/>
-      <FunctionResult :analyzer="functionCallAnalyzer" :custom-nb-col-class="propertySizeClass" :show-none="true"/>
-    </div>
-  </template>
+  <div class="action-details">
+    <Property id="actionDetailFrom" :custom-nb-col-class="propertySizeClass">
+      <template v-slot:name>From</template>
+      <template v-slot:value>
+        <EVMAddress :id="action?.caller" :address="action?.from" :entity-type="action?.caller_type"
+                    :show-type="true" :compact="!isSmallScreen"/>
+      </template>
+    </Property>
+    <Property id="actionDetailTo" :custom-nb-col-class="propertySizeClass">
+      <template v-slot:name>To</template>
+      <template v-slot:value>
+        <EVMAddress :id="action?.recipient" :address="action?.to" :entity-type="action?.recipient_type"
+                    :show-type="true" :compact="!isSmallScreen"/>
+      </template>
+    </Property>
+    <Property id="actionDetailValue" :custom-nb-col-class="propertySizeClass">
+      <template v-slot:name>Value</template>
+      <template v-slot:value>
+        <HbarAmount :amount="action?.value"/>
+      </template>
+    </Property>
+    <Property id="actionDetailGasLimit" :custom-nb-col-class="propertySizeClass">
+      <template v-slot:name>Gas Limit</template>
+      <template v-slot:value>
+        <PlainAmount :amount="action?.gas"/>
+      </template>
+    </Property>
+    <Property id="actionDetailGasUsed" :custom-nb-col-class="propertySizeClass">
+      <template v-slot:name>Gas Used</template>
+      <template v-slot:value>
+        <PlainAmount :amount="action?.gas_used"/>
+      </template>
+    </Property>
+    <FunctionError :analyzer="functionCallAnalyzer" :custom-nb-col-class="propertySizeClass" :show-none="true"/>
+    <FunctionInput :analyzer="functionCallAnalyzer" :custom-nb-col-class="propertySizeClass" :show-none="true"/>
+    <FunctionResult :analyzer="functionCallAnalyzer" :custom-nb-col-class="propertySizeClass" :show-none="true"/>
+  </div>
 </template>
 
 <!-- --------------------------------------------------------------------------------------------------------------- -->
 <!--                                                      SCRIPT                                                     -->
 <!-- --------------------------------------------------------------------------------------------------------------- -->
 
-<script lang="ts">
+<script setup lang="ts">
 
-//
-// defineComponent
-//
-
-import {computed, defineComponent, inject, onBeforeUnmount, onMounted, PropType, ref} from "vue";
+import {computed, inject, onBeforeUnmount, onMounted, PropType, ref} from "vue";
 import {ContractAction} from "@/schemas/MirrorNodeSchemas";
-import {ORUGA_MOBILE_BREAKPOINT} from "@/BreakPoints";
 import Property from "@/components/Property.vue";
 import PlainAmount from "@/components/values/PlainAmount.vue";
 import EVMAddress from "@/components/values/EVMAddress.vue";
@@ -127,42 +79,18 @@ import FunctionError from "@/components/values/FunctionError.vue";
 import HbarAmount from "@/components/values/HbarAmount.vue";
 import {ContractActionAnalyzer} from "@/utils/analyzer/ContractActionAnalyzer";
 
-export default defineComponent({
-  name: 'ContractActionDetails',
+const props = defineProps({
+  action: Object as PropType<ContractAction>
+})
 
-  components: {
-    HbarAmount,
-    FunctionError,
-    FunctionResult,
-    FunctionInput, EVMAddress, PlainAmount, Property
-  },
+const isSmallScreen = inject('isSmallScreen', ref(false))
+const propertySizeClass = 'is-one-fifth'
 
-  props: {
-    action: Object as PropType<ContractAction>
-  },
+const contractActionAnalyzer = new ContractActionAnalyzer(computed(() => props.action ?? null))
+onMounted(() => contractActionAnalyzer.mount())
+onBeforeUnmount(() => contractActionAnalyzer.unmount())
 
-  setup(props) {
-    const isTouchDevice = inject('isTouchDevice', ref(false))
-    const isSmallScreen = inject('isSmallScreen', ref(false))
-    const isMediumScreen = inject('isMediumScreen', ref(false))
-    const propertySizeClass = 'is-one-fifth'
-
-    const contractActionAnalyzer = new ContractActionAnalyzer(computed(() => props.action ?? null))
-    onMounted(() => contractActionAnalyzer.mount())
-    onBeforeUnmount(() => contractActionAnalyzer.unmount())
-
-    return {
-      isTouchDevice,
-      isSmallScreen,
-      isMediumScreen,
-      propertySizeClass,
-      ORUGA_MOBILE_BREAKPOINT,
-      functionCallAnalyzer: contractActionAnalyzer.functionCallAnalyzer,
-      functionHash: contractActionAnalyzer.functionCallAnalyzer.functionHash,
-      signature: contractActionAnalyzer.functionCallAnalyzer.signature,
-    }
-  }
-});
+const functionCallAnalyzer = contractActionAnalyzer.functionCallAnalyzer
 
 </script>
 
@@ -172,10 +100,10 @@ export default defineComponent({
 
 <style scoped>
 
-div.property-container {
+div.action-details {
   display: flex;
   flex-direction: column;
-  gap: 8px;
+  gap: 12px;
 }
 
 </style>
