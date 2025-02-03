0986a5e816768ae0b5e7464cadcfec14930a0b50
Eric Le Ponner <eric.leponner@icloud.com>
2025-02-03 12:39:21 +0100

    Re-vamped WalletInfo as WalletOptions.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

---
+++ src/components/wallet/WalletOptions.vue
@@ -0,0 +1,112 @@
+<!--
+  -
+  - Hedera Mirror Node Explorer
+  -
+  - Copyright (C) 2021 - 2024 Hedera Hashgraph, LLC
+  -
+  - Licensed under the Apache License, Version 2.0 (the "License");
+  - you may not use this file except in compliance with the License.
+  - You may obtain a copy of the License at
+  -
+  -      http://www.apache.org/licenses/LICENSE-2.0
+  -
+  - Unless required by applicable law or agreed to in writing, software
+  - distributed under the License is distributed on an "AS IS" BASIS,
+  - WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  - See the License for the specific language governing permissions and
+  - limitations under the License.
+  -
+  -->
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                     TEMPLATE                                                    -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<template>
+  <div class="wallet-options">
+
+    <!-- header -->
+    <div class="wallet-options-title">
+      Title
+    </div>
+
+    <!-- content -->
+    <div class="wallet-options-content">
+
+      <!-- EVM Address -->
+      <GroupBoxView>
+        <template #groupBoxTitle>EVM Address</template>
+        <template #default>
+          Nice EVM Address
+        </template>
+      </GroupBoxView>
+
+      <!-- Balance -->
+      <GroupBoxView>
+        <template #groupBoxTitle>Balance</template>
+        <template #default>
+          HBAR 1458.15115
+          $ 50.00
+        </template>
+      </GroupBoxView>
+
+    </div>
+
+    <!-- footer -->
+    <div class="wallet-options-footer">
+      <ButtonView>CHANGE ACCOUNT</ButtonView>
+      <ButtonView>DISCONNECT WALLET</ButtonView>
+    </div>
+
+  </div>
+</template>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                      SCRIPT                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<script setup lang="ts">
+
+import GroupBoxView from "@/elements/GroupBoxView.vue";
+import ButtonView from "@/dialogs/core/ButtonView.vue";
+
+</script>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                       STYLE                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<style scoped>
+
+div.wallet-options {
+  display: flex;
+  flex-direction: column;
+  row-gap: 24px;
+  align-items: stretch;
+}
+
+div.wallet-options-title {
+  display: flex;
+  align-items: center;
+  color: var(--text-primary);
+  border-bottom-color: var(--network-theme-color);
+  border-bottom-style: solid;
+  border-bottom-width: 1px;
+  padding-bottom: 16px;
+}
+
+div.wallet-options-content {
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  row-gap: 8px;
+}
+
+div.wallet-options-footer {
+  display: flex;
+  column-gap: 8px;
+  align-items: center;
+}
+
+
+</style>
---
+++ src/elements/GroupBoxView.vue
@@ -0,0 +1,76 @@
+<!--
+  -
+  - Hedera Mirror Node Explorer
+  -
+  - Copyright (C) 2021 - 2024 Hedera Hashgraph, LLC
+  -
+  - Licensed under the Apache License, Version 2.0 (the "License");
+  - you may not use this file except in compliance with the License.
+  - You may obtain a copy of the License at
+  -
+  -      http://www.apache.org/licenses/LICENSE-2.0
+  -
+  - Unless required by applicable law or agreed to in writing, software
+  - distributed under the License is distributed on an "AS IS" BASIS,
+  - WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  - See the License for the specific language governing permissions and
+  - limitations under the License.
+  -
+  -->
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                     TEMPLATE                                                    -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<template>
+  <div class="group-box">
+    <div v-if="slots.groupBoxTitle" class="group-box-title">
+      <slot name="groupBoxTitle"/>
+    </div>
+    <slot/>
+  </div>
+</template>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                      SCRIPT                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<script setup lang="ts">
+
+import {useSlots} from "vue";
+
+const slots = useSlots()
+
+</script>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                       STYLE                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<style scoped>
+
+div.group-box {
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  border-radius: 16px;
+  border-width: 1px;
+  border-color: var(--table-border);
+  border-style: solid;
+  padding: 16px;
+  row-gap: 8px;
+  background-color: var(--background-primary);
+  color: var(--text-primary);
+}
+
+div.group-box-title {
+  display: flex;
+  align-items: center;
+  column-gap: 8px;
+  color: var(--text-secondary);
+  font-family: 'Inter', sans-serif;
+  font-weight: 400;
+  font-size: 12px;
+}
+
+</style>
