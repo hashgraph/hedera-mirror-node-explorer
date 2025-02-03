d94064142fafee788da178d6219e5bc198664d98
Simon Viénot <simon.vienot@icloud.com>
2025-02-03 19:19:03 +0100

    Add MobileMenuButton (for further use).
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

---
+++ src/components/page/header/MobileMenuButton.vue
@@ -0,0 +1,52 @@
+<!--
+  -
+  - Hedera Mirror Node Explorer
+  -
+  - Copyright (C) 2021 - 2023 Hedera Hashgraph, LLC
+  -
+  - Licensed under the Apache License, Version 2.0 (the "License");
+  - you may not use this file except in compliance with the License.
+  - You may obtain a copy of the License at
+  -
+  -      http://www.apache.org/licenses/LICENSE-2.0
+  -
+  - Unless required by applicable law or agreed to in writing, software
+  - distributed under the License is distributed on an "AS IS" BASIS,
+  - WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  - See the License for the specific language governing permissions and
+  - limitations under the License.
+  -
+  -->
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                     TEMPLATE                                                    -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<template>
+  <router-link v-if="name !== 'MobileMenu' && name !== 'MobileSearch'" :to="routeManager.makeRouteToMobileMenu(name)">
+    <Menu :size="18" style="color: var(--text-primary)"/>
+  </router-link>
+  route: {{ name }} network: {{ network }}
+</template>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                      SCRIPT                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<script setup lang="ts">
+
+import {routeManager} from "@/router.ts"
+import {Menu} from 'lucide-vue-next';
+
+const name = routeManager.currentRoute
+const network = routeManager.currentNetwork
+
+</script>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                       STYLE                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<style scoped>
+
+</style>
