ac41df632e6d117138110c7392ca28af393bb9bf
Simon Viénot <simon.vienot@icloud.com>
2025-02-21 17:35:34 +0100

    Convert ArrowSegment to script setup and make it re-render the SVG when switching dark/light mode.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- src/components/transfer_graphs/ArrowSegment.vue
+++ src/components/transfer_graphs/ArrowSegment.vue
@@ -30,67 +30,63 @@
 <!--                                                      SCRIPT                                                     -->
 <!-- --------------------------------------------------------------------------------------------------------------- -->
 
-<script lang="ts">
+<script setup lang="ts">
 
-import {defineComponent, ref, watch} from "vue";
+import {ref, watch} from "vue";
 import {makeTransferSVG} from "@/utils/SVGUtils";
 import ResizeObserver from "resize-observer-polyfill";
+import {ThemeController} from "@/components/ThemeController.ts";
 
-export default defineComponent({
-  name: "ArrowSegment",
-
-  props: {
-    sourceCount: {
-      type: Number,
-      default: 1
-    },
-    destCount: {
-      type: Number,
-      default: 1
-    },
-    rowIndex: {
-      type: Number,
-      default: 0
-    },
-    compact: {
-      type: Boolean,
-      default: false,
-    }
+const props = defineProps({
+  sourceCount: {
+    type: Number,
+    default: 1
   },
-
-  setup(props) {
-    const rootElement = ref<SVGSVGElement | null>(null)
-    const parentElement = ref<HTMLElement | null>(null)
-    const svgContent = ref<string | null>(null)
-
-    watch(rootElement, () => {
-      parentElement.value = rootElement.value?.parentElement ?? null
-    })
-
-    const resizeObserver = new ResizeObserver(() => {
-      updateSvgContent()
-    })
-    watch(parentElement, (newValue, oldValue) => {
-      if (oldValue !== null) {
-        resizeObserver.unobserve(oldValue)
-      }
-      if (newValue !== null) {
-        resizeObserver.observe(newValue)
-        updateSvgContent()
-      }
-    })
-
-    const updateSvgContent = () => {
-      if (parentElement.value != null) {
-        const bb = parentElement.value.getBoundingClientRect();
-        const dy = props.compact ? 9.5 : 11.5
-        svgContent.value = makeTransferSVG(bb.width, bb.height, dy, props.sourceCount, props.destCount, props.rowIndex)
-      }
-    }
-
-    return {rootElement, svgContent}
+  destCount: {
+    type: Number,
+    default: 1
+  },
+  rowIndex: {
+    type: Number,
+    default: 0
+  },
+  compact: {
+    type: Boolean,
+    default: false,
   }
-});
+})
+
+const rootElement = ref<SVGSVGElement | null>(null)
+const parentElement = ref<HTMLElement | null>(null)
+const svgContent = ref<string | null>(null)
+
+watch(rootElement, () => {
+  parentElement.value = rootElement.value?.parentElement ?? null
+})
+
+const resizeObserver = new ResizeObserver(() => {
+  updateSvgContent()
+})
+watch(parentElement, (newValue, oldValue) => {
+  if (oldValue !== null) {
+    resizeObserver.unobserve(oldValue)
+  }
+  if (newValue !== null) {
+    resizeObserver.observe(newValue)
+    updateSvgContent()
+  }
+})
+
+const updateSvgContent = () => {
+  if (parentElement.value != null) {
+    const bb = parentElement.value.getBoundingClientRect();
+    const dy = props.compact ? 9.5 : 11.5
+    svgContent.value = makeTransferSVG(bb.width, bb.height, dy, props.sourceCount, props.destCount, props.rowIndex)
+  }
+}
+
+const darkSelected = ThemeController.inject().darkSelected
+watch(darkSelected, updateSvgContent, {immediate: true})
 
 </script>
 
