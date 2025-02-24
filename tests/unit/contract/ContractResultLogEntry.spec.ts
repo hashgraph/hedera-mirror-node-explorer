d82d294e904fe9ed62e4c0e0d12dbe94b7f1023e
Eric Le Ponner <eric.leponner@icloud.com>
2025-02-24 15:06:30 +0100

    Adjusted ContractResultLogEntry.spec.ts to new UI.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- tests/unit/contract/ContractResultLogEntry.spec.ts
+++ tests/unit/contract/ContractResultLogEntry.spec.ts
@@ -30,6 +30,7 @@ import {HMSF} from "@/utils/HMSF";
 import Oruga from "@oruga-ui/oruga-next";
 import ContractResultLogEntry from "@/components/contract/ContractResultLogEntry.vue";
 import {SAMPLE_LOG_RESULT, SAMPLE_SOURCIFY_RESPONSE} from "../utils/analyzer/ContractLogAnalyzer.spec";
+import {fetchGetURLs} from "../MockUtils";
 
 /*
     Bookmarks
@@ -41,7 +42,7 @@ HMSF.forceUTC = true
 
 describe("ContractResultLogEntry.vue", () => {
 
-    it.skip("Should display the contract result and logs, given consensus timestamp", async () => {
+    it("Should display the contract result and logs, given consensus timestamp", async () => {
 
         await router.push("/") // To avoid "missing required param 'network'" error
 
@@ -65,11 +66,18 @@ describe("ContractResultLogEntry.vue", () => {
         });
         await flushPromises()
 
+        expect(fetchGetURLs(mock)).toStrictEqual([
+            "api/v1/contracts/" + SAMPLE_CONTRACT.contract_id,
+            "api/v1/contracts/" + SAMPLE_CONTRACT.evm_address,
+            "http://localhost:3000/files/any/295/" + SAMPLE_CONTRACT.evm_address,
+            "api/v1/accounts/" + SAMPLE_CONTRACT.evm_address,
+        ])
+
         expect(wrapper.get("#transactionHash").text()).toBe("Transaction Hashc7b2 ecff defd ec56 b809 f93b ef6c 8528 dad2 2e58 a02a 33c2 0d27 5f2e f279 36d5 6b1e d64b 53f9 20fb ac85 d9ca da4a 4e78Copy")
         expect(wrapper.get("#blockNumber").find("a").exists()).toBeTruthy
         expect(wrapper.get("#blockNumber").get("a").text()).toBe('9')
         expect(wrapper.get("#blockNumber").text()).toBe("Block9")
-        expect(wrapper.get("#address").text()).toBe("Address0x00000000000000000000000000000000000b70cfCopy(TestEvent)")
+        expect(wrapper.get("#address").text()).toBe("Address0x00…0b70cfCopy(TestEvent)")
         expect(wrapper.get("#Args").text()).toBe("LogsFlightEvent (string phase, int256 airspeed, int256 verticalSpeed)Topic 0  signature hash0x01f789d670afa3030578cc570ac4d43ace6f1575dd6c395a711e72a30051efd2string  phaseHolding pointint256  airspeed0int256  verticalSpeed0")
         expect(wrapper.get("#logArg_phase").text()).toBe("string  phaseHolding point")
         expect(wrapper.get("#logArg_airspeed").text()).toBe("int256  airspeed0")
