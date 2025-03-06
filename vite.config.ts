faf0a6f10635bd16c17f01a8685c398f9a792e90
Simon Viénot <simon.vienot@icloud.com>
2025-03-06 16:41:03 +0100

    Roll-back recent changes to tagAndCommitHash in vite.config.ts.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- vite.config.ts
+++ vite.config.ts
@@ -5,22 +5,10 @@ import vue from '@vitejs/plugin-vue'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import {nodePolyfills} from 'vite-plugin-node-polyfills'
 import * as child from "child_process"
-import {readFileSync} from "fs"
 
 const commitHash = child.execSync("git rev-parse --short HEAD").toString() //i.e., 706e821
-const tagAndCommitHash = resolveVersion() //i.e., v23.5.0-1-g706e821
+const tagAndCommitHash = child.execSync("git describe --always --tags").toString() //i.e., v23.5.0-1-g706e821
 const buildDate = new Date().toUTCString()
-
-function resolveVersion() {
-  try {
-    const shortHash = child.execSync("git rev-parse --short HEAD").toString().trim()
-    const packageJson = JSON.parse(readFileSync('./package.json', 'utf8'))
-    return `v${packageJson.version}+${shortHash}` || child.execSync("git describe --always --tags").toString()
-  } catch (e) {
-    console.error('Could not read package.json')
-    return child.execSync("git describe --always --tags").toString()
-  }
-}
 
 // https://vitejs.dev/config/
 export default defineConfig({
