d3d51e11b918ad6a69cce2b0e14ed8f25d2fdea9
Simon Viénot <simon.vienot@icloud.com>
2025-03-07 08:32:59 +0100

    chore: replace copyright notice by SPDX-License-Identifier (#1704)
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- src/dialogs/common/AccountTextFieldController.ts
+++ src/dialogs/common/AccountTextFieldController.ts
@@ -1,31 +1,10 @@
-/*-
- *
- * Hedera Mirror Node Explorer
- *
- * Copyright (C) 2021 - 2024 Hedera Hashgraph, LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
+// SPDX-License-Identifier: Apache-2.0
 
 import {computed, Ref} from "vue";
 import {AccountByIdCache} from "@/utils/cache/AccountByIdCache.ts";
 import {AccountBalanceTransactions} from "@/schemas/MirrorNodeSchemas.ts";
 import {NetworkConfig} from "@/config/NetworkConfig.ts";
-import {
-    EntityTextFieldController,
-    EntityTextFieldState
-} from "@/dialogs/common/EntityTextFieldController.ts";
+import {EntityTextFieldController, EntityTextFieldState} from "@/dialogs/common/EntityTextFieldController.ts";
 import {EntityLookup} from "@/utils/cache/base/EntityCache.ts";
 
 export class AccountTextFieldController {
--- tests/unit/dashboard/MainDashboard.spec.ts
+++ tests/unit/dashboard/MainDashboard.spec.ts
@@ -1,31 +1,10 @@
-/*-
- *
- * Hedera Mirror Node Explorer
- *
- * Copyright (C) 2021 - 2024 Hedera Hashgraph, LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
+// SPDX-License-Identifier: Apache-2.0
 
 import {describe, expect, test} from 'vitest'
 import {flushPromises, mount} from "@vue/test-utils"
 import router from "@/router";
 import axios from "axios";
-import {
-    SAMPLE_NETWORK_EXCHANGERATE,
-    SAMPLE_NETWORK_SUPPLY,
-} from "../Mocks";
+import {SAMPLE_NETWORK_EXCHANGERATE, SAMPLE_NETWORK_SUPPLY,} from "../Mocks";
 import MainDashboard from "@/pages/MainDashboard.vue";
 import MockAdapter from "axios-mock-adapter";
 import Oruga from "@oruga-ui/oruga-next";
