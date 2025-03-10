7e9e15d1114085975433f6deccf296e738ca31a6
Simon Viénot <simon.vienot@icloud.com>
2025-03-10 19:13:06 +0100

    Improve user feedback in sourcify-setup.sh and ignore certificate files in git.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- scripts/sourcify-setup.sh
+++ scripts/sourcify-setup.sh
@@ -3,8 +3,8 @@
 TARGET_DIR=$(npm prefix)/sourcify-setup
 
 if [[ ! -d "$TARGET_DIR" ]]; then
-  echo "⚠️ Did not find the sourcify-setup directory under project root."
-  echo "Do not run this script directly and make sure to use the npm command sourcify:check-setup"
+  echo "❌️ Did not find the sourcify-setup directory under project root."
+  echo "   --> Do not run this script directly and make sure to use the npm command sourcify:check-setup"
   exit 1
 fi
 
@@ -16,12 +16,12 @@ HOSTS_FILE="/etc/hosts"
 
 # Check if the hostname is already mapped to 127.0.0.1
 if grep -qE "^127\.0\.0\.1\s+.*\b${HOSTNAME}\b" "$HOSTS_FILE"; then
-    echo "✅ Host ${HOSTNAME} is correctly mapped to 127.0.0.1 in $HOSTS_FILE."
+    echo "✅  Host ${HOSTNAME} is correctly mapped to 127.0.0.1 in $HOSTS_FILE."
     result=0
 else
-    echo "⚠️ Host ${HOSTNAME} is NOT correctly mapped to 127.0.0.1 in /etc/hosts"
-    echo "--> Edit /etc/hosts or execute the following command:"
-    echo "    sudo bash -c 'echo "127.0.0.1       repository.local" >> /etc/hosts'"
+    echo "❌️ Host ${HOSTNAME} is NOT correctly mapped to 127.0.0.1 in /etc/hosts"
+    echo "   --> Edit /etc/hosts or execute the following command:"
+    echo "   sudo bash -c 'echo "127.0.0.1       repository.local" >> /etc/hosts'"
     result=1
 fi
 
@@ -42,30 +42,34 @@ if [[ -f "$CERT_FILE" && -f "$KEY_FILE" ]]; then
   CURRENT_EPOCH=$(date -j +%s)
 
   if [ "$NOT_AFTER_EPOCH" -ge "$CURRENT_EPOCH" ]; then
-    echo "✅ Valid self-signed certificate for $DOMAIN is already installed"
+    echo "✅  Valid self-signed certificate for $DOMAIN is already installed"
     result=0
   else
     echo "❌️ Certificate is not valid"
-    echo "--> Clean up remaining certificate and/or key files and re-run this script"
+    echo "   --> Clean up remaining files under:"
+    echo "   $CERT_DIR/"
     result=1
   fi
+
 elif [[ ! -f "$CERT_FILE" && -f "$KEY_FILE" ]]; then
   echo "❌️ Certificate file missing"
-  echo "--> Clean up remaining key file and re-run this script"
+  echo "   --> Clean up remaining key file:"
+  echo "   $KEY_FILE"
   result=1
 elif [[ ! -f "$KEY_FILE" && -f "$CERT_FILE" ]]; then
   echo "❌️ Key file missing"
-  echo "--> Clean up remaining certificate file and re-run this script"
+  echo "   --> Clean up remaining certificate file:"
+  echo "   $CERT_FILE"
   result=1
 else
-  echo "✅ Generating self-signed certificate for $DOMAIN..."
+  echo "✅  Generating self-signed certificate for $DOMAIN..."
   mkdir -p "$CERT_DIR"
   openssl req -x509 -nodes -days "$DAYS" -newkey rsa:2048 \
-    -keyout "$KEY_FILE" \
-    -out "$CERT_FILE" \
-    -subj "/CN=$DOMAIN"
-  echo "Certificate : $CERT_FILE"
-  echo "Private key : $KEY_FILE"
+      -keyout "$KEY_FILE" \
+      -out "$CERT_FILE" \
+      -subj "/CN=$DOMAIN"
+  echo "   Certificate : $CERT_FILE"
+  echo "   Private key : $KEY_FILE"
   result=0
 fi
 
@@ -74,11 +78,11 @@ echo
 ### CHECK WHETHER DOCKER IS RUNNING
 
 if docker info > /dev/null 2>&1; then
-  echo "✅ Docker is running"
+  echo "✅  Docker is running"
     result=0
 else
-  echo "❌ Docker is not running!"
-  echo "--> Make sure docker is running -- e.g. by starting Docker Desktop"
+  echo "❌️ Docker is not running!"
+  echo "   --> Make sure docker is running -- e.g. by starting Docker Desktop"
   result=1
 fi
 
@@ -87,11 +91,11 @@ echo
 ### CHECK WHETHER LOGGED INTO ghcr.io
 
 if [[ $(cat ~/.docker/config.json | jq -r '.auths["ghcr.io"]? // empty') ]]; then
-  echo "✅ You are logged into ghcr.io"
+  echo "✅  You are logged into ghcr.io"
 else
-  echo "❌ You are not logged into ghcr.io"
-  echo "--> Make sure you have created a Personal Access Token in your account on GitHub and execute the following:"
-  echo "    echo <personal-access-token> | docker login ghcr.io -u <github-user-name> --password-stdin"
+  echo "❌️ You are not logged into ghcr.io"
+  echo "   --> Make sure you have created a Personal Access Token in your account on GitHub and execute the following:"
+  echo "   echo <personal-access-token> | docker login ghcr.io -u <github-user-name> --password-stdin"
   exit 1
 fi
 
