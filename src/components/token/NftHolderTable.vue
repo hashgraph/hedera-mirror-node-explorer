0c0b79f251608aa9bb0626bc824e216643fbbba3
Eric Le Ponner <eric.leponner@icloud.com>
2025-03-12 19:10:17 +0100

    NftHolderTable now uses TableView.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- src/components/token/NftHolderTable.vue
+++ src/components/token/NftHolderTable.vue
@@ -7,86 +7,69 @@
 <template>
 
   <div id="nft-holder-table">
-    <o-table
-        :data="nfts"
-        :loading="loading"
-        :paginated="paginated"
-        backend-pagination
-        pagination-order="centered"
-        :range-before="1"
-        :range-after="1"
-        :total="total"
-        v-model:current-page="currentPage"
-        :per-page="perPage"
-        @page-change="onPageChange"
 
+    <TableView
+        :controller="props.controller"
+        :clickable="true"
+        :page-size-storage-key="AppStorage.BLOCK_TABLE_PAGE_SIZE_KEY"
         @cell-click="handleClick"
-        :hoverable="true"
-        :narrowed="true"
-        :striped="true"
-        :mobile-breakpoint="ORUGA_MOBILE_BREAKPOINT"
-
-        aria-current-label="Current page"
-        aria-next-label="Next page"
-        aria-page-label="Page"
-        aria-previous-label="Previous page"
-        customRowKey="serial_number"
     >
-      <o-table-column v-slot="props" field="image" label="PREVIEW">
-        <NftCell
-            class="w400"
-            :token-id="props.row.token_id"
-            :serial-number="props.row.serial_number"
-            :property="NftCellItem.image"/>
-      </o-table-column>
 
-      <o-table-column v-slot="props" field="serial" label="#">
-        <span class="serial-number">
-          {{ props.row.serial_number }}
-        </span>
-      </o-table-column>
-
-      <o-table-column v-slot="props" field="name" label="NAME">
-        <NftCell
-            :token-id="props.row.token_id"
-            :serial-number="props.row.serial_number"
-            :property="NftCellItem.name"/>
-      </o-table-column>
-
-      <o-table-column v-slot="props" field="creator" label="CREATOR">
-        <NftCell
-            :token-id="props.row.token_id"
-            :serial-number="props.row.serial_number"
-            :property="NftCellItem.creator"/>
-      </o-table-column>
-
-      <o-table-column v-slot="props" field="account_id" label="OWNER">
-        <AccountIOL :account-id="props.row.account_id"/>
-      </o-table-column>
-
-      <o-table-column v-slot="props" field="description" label="DESCRIPTION">
-        <NftCell
-            :token-id="props.row.token_id"
-            :serial-number="props.row.serial_number"
-            :property="NftCellItem.description"/>
-      </o-table-column>
-
-      <template v-slot:bottom-left>
-        <TablePageSize
-            v-model:size="perPage"
-            :storage-key="AppStorage.NFT_HOLDER_TABLE_PAGE_SIZE_KEY"
-        />
+      <template #tableHeaders>
+        <TableHeaderView>PREVIEW</TableHeaderView>
+        <TableHeaderView>#</TableHeaderView>
+        <TableHeaderView>NAME</TableHeaderView>
+        <TableHeaderView>CREATOR</TableHeaderView>
+        <TableHeaderView>OWNER</TableHeaderView>
+        <TableHeaderView>DESCRIPTION</TableHeaderView>
       </template>
 
-    </o-table>
+      <template #tableCells="nft">
 
-    <TablePageSize
-        v-if="!paginated && showPageSizeSelector"
-        v-model:size="perPage"
-        style="width: 116px; margin-left: 4px"
-    />
+        <TableDataView>
+          <NftCell
+              class="w400"
+              :token-id="nft.token_id"
+              :serial-number="nft.serial_number"
+              :property="NftCellItem.image"/>
+        </TableDataView>
 
-    <EmptyTable v-if="!nfts.length"/>
+        <TableDataView>
+          <span class="serial-number">
+            {{ nft.serial_number }}
+          </span>
+        </TableDataView>
+
+        <TableDataView>
+          <NftCell
+              :token-id="nft.token_id"
+              :serial-number="nft.serial_number"
+              :property="NftCellItem.name"/>
+        </TableDataView>
+
+        <TableDataView>
+          <NftCell
+              :token-id="nft.token_id"
+              :serial-number="nft.serial_number"
+              :property="NftCellItem.creator"/>
+        </TableDataView>
+
+        <TableDataView>
+          <AccountIOL :account-id="nft.account_id"/>
+        </TableDataView>
+
+        <TableDataView>
+          <NftCell
+              :token-id="nft.token_id"
+              :serial-number="nft.serial_number"
+              :property="NftCellItem.description"/>
+        </TableDataView>
+
+      </template>
+
+
+    </TableView>
+
   </div>
 
 </template>
@@ -99,14 +82,14 @@
 
 import {PropType} from 'vue';
 import {Nft} from "@/schemas/MirrorNodeSchemas";
-import {ORUGA_MOBILE_BREAKPOINT} from "@/BreakPoints";
-import EmptyTable from "@/components/EmptyTable.vue";
 import {NftHolderTableController} from "@/components/token/NftHolderTableController";
 import {routeManager} from "@/router";
 import AccountIOL from "@/components/values/link/AccountIOL.vue";
 import NftCell, {NftCellItem} from "@/components/token/NftCell.vue";
-import TablePageSize from "@/components/transaction/TablePageSize.vue";
 import {AppStorage} from "@/AppStorage";
+import TableView from "@/tables/TableView.vue";
+import TableHeaderView from "@/tables/TableHeaderView.vue";
+import TableDataView from "@/tables/TableDataView.vue";
 
 const props = defineProps({
   controller: {
@@ -115,20 +98,11 @@ const props = defineProps({
   },
 })
 
-const handleClick = (n: Nft, c: unknown, i: number, ci: number, event: MouseEvent,) => {
+const handleClick = (n: Nft, event: MouseEvent,) => {
   if (n.token_id && n.serial_number) {
     routeManager.routeToSerial(n.token_id, n.serial_number, event);
   }
 };
-
-const nfts = props.controller.rows
-const loading = props.controller.loading
-const total = props.controller.totalRowCount
-const currentPage = props.controller.currentPage
-const onPageChange = props.controller.onPageChange
-const perPage = props.controller.pageSize
-const paginated = props.controller.paginated
-const showPageSizeSelector = props.controller.showPageSizeSelector
 
 </script>
 
