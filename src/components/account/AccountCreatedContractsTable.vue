8f5815f76c79635982a005432cf698cbf42f4f5b
Eric Le Ponner <eric.leponner@icloud.com>
2025-03-13 14:34:47 +0100

    AccountCreatedContractsTable.vue now uses TableView.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- src/components/account/AccountCreatedContractsTable.vue
+++ src/components/account/AccountCreatedContractsTable.vue
@@ -6,63 +6,39 @@
 
 <template>
 
-  <o-table
-      v-model:current-page="currentPage"
-      :data="transactions"
-      :hoverable="true"
-      :loading="loading"
-      :mobile-breakpoint="ORUGA_MOBILE_BREAKPOINT"
-      :narrowed="true"
-      :paginated="paginated"
-      pagination-order="centered"
-      :range-before="1"
-      :range-after="1"
-      :per-page="perPage"
-      :striped="true"
-
-      :total="total"
-      aria-current-label="Current page"
-      aria-next-label="Next page"
-      aria-page-label="Page"
-
-      aria-previous-label="Previous page"
-      backend-pagination
-      customRowKey="consensus_timestamp"
-      default-sort="consensus_timestamp"
-      @page-change="onPageChange"
+  <TableView
+      :controller="props.controller"
+      :clickable="true"
       @cell-click="handleClick"
   >
 
-    <o-table-column v-slot="props" field="contract_id" label="ID">
-      <div class="entity-id">
-        {{ props.row.entity_id }}
-      </div>
-    </o-table-column>
+    <template #tableHeaders>
 
-    <o-table-column v-slot="props" field="contract_name" label="CONTRACT NAME">
-      <ContractName :contract-id="props.row.entity_id"/>
-    </o-table-column>
+      <TableHeaderView>ID</TableHeaderView>
+      <TableHeaderView>CONTRACT NAME</TableHeaderView>
+      <TableHeaderView>CREATE</TableHeaderView>
 
-    <o-table-column v-slot="props" field="created" label="CREATE">
-      <TimestampValue v-bind:timestamp="props.row.consensus_timestamp"/>
-    </o-table-column>
-
-    <template v-slot:bottom-left>
-      <TablePageSize
-          v-model:size="perPage"
-          :storage-key="AppStorage.ACCOUNT_OPERATION_TABLE_PAGE_SIZE_KEY"/>
     </template>
 
-  </o-table>
+    <template #tableCells="transaction">
 
-  <TablePageSize
-      v-if="!paginated && showPageSizeSelector"
-      v-model:size="perPage"
-      :storage-key="AppStorage.ACCOUNT_OPERATION_TABLE_PAGE_SIZE_KEY"
-      style="width: 116px; margin-left: 4px"
-  />
+      <TableDataView>
+        <div class="entity-id">
+          {{ transaction.entity_id }}
+        </div>
+      </TableDataView>
 
-  <EmptyTable v-if="!transactions.length"/>
+      <TableDataView>
+        <ContractName :contract-id="transaction.entity_id ?? ''"/>
+      </TableDataView>
+
+      <TableDataView>
+        <TimestampValue v-bind:timestamp="transaction.consensus_timestamp"/>
+      </TableDataView>
+
+    </template>
+
+  </TableView>
 
 </template>
 
@@ -76,12 +52,11 @@ import {onBeforeUnmount, onMounted, PropType} from 'vue';
 import {Transaction} from "@/schemas/MirrorNodeSchemas";
 import TimestampValue from "@/components/values/TimestampValue.vue";
 import {routeManager} from "@/router";
-import {ORUGA_MOBILE_BREAKPOINT} from "@/BreakPoints";
-import EmptyTable from "@/components/EmptyTable.vue";
 import {TransactionTableController} from "@/components/transaction/TransactionTableController";
 import ContractName from "@/components/values/ContractName.vue";
-import TablePageSize from "@/components/transaction/TablePageSize.vue";
-import {AppStorage} from "@/AppStorage";
+import TableDataView from "@/tables/TableDataView.vue";
+import TableHeaderView from "@/tables/TableHeaderView.vue";
+import TableView from "@/tables/TableView.vue";
 
 const props = defineProps({
   controller: {
@@ -93,18 +68,9 @@ const props = defineProps({
 onMounted(() => props.controller.mount())
 onBeforeUnmount(() => props.controller.unmount())
 
-const handleClick = (t: Transaction, c: unknown, i: number, ci: number, event: MouseEvent) => {
+const handleClick = (t: Transaction, event: MouseEvent) => {
   routeManager.routeToContract(t.entity_id!, event)
 }
-
-const transactions = props.controller.rows
-const loading = props.controller.loading
-const total = props.controller.totalRowCount
-const currentPage = props.controller.currentPage
-const onPageChange = props.controller.onPageChange
-const perPage = props.controller.pageSize
-const paginated = props.controller.paginated
-const showPageSizeSelector = props.controller.showPageSizeSelector
 
 </script>
 
